dce55dcb660a97edf0573eeb36d22799ef317a35
Zyx Rhythm <zyxrhythm@gmail.com>
2018-12-26 06:13:18 +0800

    Update news.sh

--- news.sh
+++ news.sh
@@ -54,7 +54,7 @@ pre{
 -----------------------
 
 Noted Revisions:
--Disabled unecessary services (bind9, mysql, nginx, webmin, postfix, php7.0-fpm), and focused the server to only serve BBC pages (hopefully will take care of the 500 Internal server errors), and will prevent chinese attackers from executing randomly named php files which causes alot of access and error logs. 
+-Disabled unecessary services (bind9, mysql, nginx, webmin, postfix, php7.0-fpm), and focused the server to only serve BBC pages (hopefully will take care of the 500 Internal server errors from failures to allocate memory to apache processes), and will prevent chinese attackers from executing randomly named php files which causes alot of access and error logs. 
 -Speed and stability fixes: (removed full url links from the codes removing the need for another DNS resolution for incresed speed, added a cron that will restart apache2 every hour for statibility -to temporarily handle the 500 server internal Errors)
 -Removed extra whois result from all ccTLDs.
 -Added a domain validity check at the start of the BBC special script process.
