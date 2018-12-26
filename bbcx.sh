f7f7f7f6b25a7ff1d7d0759fd998c35d9a79ab63
Zyx Rhythm <zyxrhythm@gmail.com>
2018-12-27 02:34:14 +0800

    Update bbcx.sh

--- bbcx.sh
+++ bbcx.sh
@@ -36,6 +36,16 @@ cat <<EOX
   gtag('js', new Date());
   gtag('config', 'UA-32625644-1');
 </script>
+
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-3422469360028179",
+    enable_page_level_ads: true
+  });
+</script>
+
+
 EOX
 
 #CSS
