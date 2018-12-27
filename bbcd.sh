025872a326f358efcb5d9cc8ece27f5123ee7071
Zyx Rhythm <zyxrhythm@gmail.com>
2018-12-28 02:28:16 +0800

    Update bbcd.sh

--- bbcd.sh
+++ bbcd.sh
@@ -54,13 +54,9 @@ p  {
 
 h1 {font-family: verdana;
 	font-size: 70%;
-
 }
 
-
 body {background-color:black;color:white
-
-
 }
 
 pre{
@@ -101,7 +97,6 @@ gtldlist='+(aarp|abarth|abb|abbott|abbvie|abc|able|abogado|abudhabi|academy|acce
 #list of supported ccTLDs
 cctldlist='+(ac|ad|ae|af|ag|ai|al|am|ao|aq|ar|as|at|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cu|cv|cw|cx|cy|cz|de|dj|dk|dm|do|dz|ec|ee|eg|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sk|sl|sm|sn|so|sr|ss|st|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|za|zm|zw)'
 
-
 #butchers the QUERY_STRING for the domain the name server and the record type and stores them in variables 
 domain=$(grep -oP '(?<=domain=).*?(?=&)' <<< "$qs");
 DNSR=$(grep -oP '(?<=record=).*?(?=&)' <<< "$qs");
@@ -133,7 +128,6 @@ tld=$( echo $domain | rev | cut -d "." -f1 | rev );
 #checks if the domain is a gtld
 case $tld in
    $gtldlist)
-
 
 #prints the dig results for gtlds
 zyxgd=$(dig +noall +answer $DNSR $domain $qns);
@@ -177,7 +171,6 @@ esac
 echo '</p>'
 
 echo '</div>'
-
 
 echo '<p> <a href="/cgi-bin/bbc.sh" > << back | track</a> </p>' 
 
