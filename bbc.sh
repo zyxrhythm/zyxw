2c4eaaaa4a7b02ac36281ef937978c09c6aa420b
Zyx Rhythm <zyxrhythm@gmail.com>
2019-01-04 04:56:56 +0800

    Update bbc.sh

--- bbc.sh
+++ bbc.sh
@@ -65,12 +65,6 @@ label { font-family: Trebuchet MS; font-size: 110%;
 button { font-family: Trebuchet MS; font-size: 70%; 
 }
 
-option { font-family: verdana; font-size: 90%; text-align: center;
-}
-
-select { font-family: verdana; font-size: 90%; text-align: center;
-}
-
 footer { font-family: Georgia; font-size: 100%;
 }
 
