3338dd6333f040e34252965d7a881297c957756b
Zyx Rhythm <zyxrhythm@gmail.com>
2019-01-05 07:27:39 +0800

    Create EPP.sh

---
+++ EPP.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+echo "Content-type: text/html"
+echo ""
+cat <<EOT
+
+<!DOCTYPE html>
+<html>
+<link rel="icon" type="image/png" href="/icon.png" />
+<head>
+<title>BBC ( v 0.7 )</title>
+
+<meta name="description" content="BigBlackCactus.com (BBC) is a website that can fetch the whois information of a domain, dig DNS records of domains and sub domains from name servers, can also do a simple check to identify about the SSL certificate issued for a domain/sub domain name.">
+<meta name="keywords" content="DIG, DNS, WHOIS, SSL CHECK">
+<meta name="author" content="Zyx Rhythm">
+ 
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-32625644-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-32625644-1');
+</script>
+
+
+<center>
+<p><h1><a href="/cgi-bin/bbc.sh">Big Black Cactus</a></h1></p>
+</center>
+<style>
+h1 {    font-family: courier new;
+    font-size: 150%;
+}
+
+a:link { color: green; }
+a:visited { color: green; }
+a:hover { color: green; }
+a:active { color: green; }
+
+body	{background-color:black;color:green
+
+}
+
+p	{	font-family: calibri;
+    font-size: 100%;
+}
+
+footer	{font-family: calibri;
+	font-size: 60%;
+}
+
+article	{font-family: arial;
+	font-size: 43%;
+}
+
+pre{
+    white-space: pre-wrap;font-size: 100%;
+}
+
+</style>
+</head>
+
+<body>
+
+<left>
+<br>
+<br>
+<p>
+<pre>
+
+
+
+</pre>
+<br>
+</p>
+
+</left>
+</body>
+
+</html>
+
+EOT
+
+exit 0;
