667ad468c7b0741cc7d57ffc58e0d08fbd512bfc
Zyx Rhythm <zyxrhythm@gmail.com>
2019-01-10 03:24:07 +0800

    Update bbcx.sh

--- etc/bbcx.sh
+++ etc/bbcx.sh
@@ -688,7 +688,7 @@ typicalwhoisresult=$(whois $domain);
 whoisservergrep=$(echo "$typicalwhoisresult" | grep -i -e "WHOIS Server" | sort -u );
 whoisserver=$(echo "$whoisservergrep" | cut -f2 -d":" | tr -d '\040\011\012\015' );
 
-if [[ "$whoisserver" !== "http://api.fastdomain.com/cgi/whois" ]]; then
+#if [[ "$whoisserver" !== "http://api.fastdomain.com/cgi/whois" ]]; then
 
 semifinale=$(whois $domain -h "$whoisserver" );
 rese=$(echo "$semifinale" | grep -i -e "reseller");
@@ -723,8 +723,8 @@ echo "<br>"
 echo '<hr>'
 echo '<p> <a href="/cgi-bin/bbc.sh" ><< back | track</a> </p>'
 
-else
-echo "<br> click <a href='http://api.fastdomain.com/cgi/whois?domain=$domain' target='_blank'>here</a> for the raw whois info from the registrar.<br>"
+#else
+#echo "<br> click <a href='http://api.fastdomain.com/cgi/whois?domain=$domain' target='_blank'>here</a> for the raw whois info from the registrar.<br>"
 
 
 #the end of body
