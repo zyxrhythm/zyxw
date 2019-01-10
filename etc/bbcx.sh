25423c8b7bc971d1040d9a01945c69d9b84df15b
Zyx Rhythm <zyxrhythm@gmail.com>
2019-01-11 02:12:14 +0800

    Update bbcx.sh

--- etc/bbcx.sh
+++ etc/bbcx.sh
@@ -239,7 +239,7 @@ echo "<a href="/cgi-bin/eppstatuscodes.sh" rel="noopener noreferrer" target="_bl
 echo "<br/>"
 while IFS= read -r line
 do
-   echo  "</br> <a href=/cgi-bin/eppstatuscodes.sh#${line#*#} target=_blank > [?]</a> ${line#*#}";
+   echo  "</br> <a href=/cgi-bin/eppstatuscodes.sh#${line#*#} target=_blank style="color:azure"> [?]</a> ${line#*#}";
 done < <(printf '%s\n' "$dstat");
 
 #print the domain creation and expiration dates
