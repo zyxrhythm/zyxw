c5210662b0a897cb564b6273eda82ae53378a784
Zyx Rhythm <zyxrhythm@gmail.com>
2019-01-12 03:39:58 +0800

    Update eppstatuscodes.sh

--- eppstatuscodes.sh
+++ eppstatuscodes.sh
@@ -102,6 +102,15 @@ footer{color: green; text-align: center; font-family: verdana; font-size: 65%;
 </tbody>
 </table>
 
+
+
+<table>
+<tbody>
+<tr class="dark">
+		<th>Status Code</th>
+		<th>What does it mean?</th>
+		<th>Should you do something?</th>
+	</tr>
 	<tr>
 		<td>
 <a name="inactive" id="inactive"></a>inactive</td>
@@ -110,6 +119,18 @@ footer{color: green; text-align: center; font-family: verdana; font-size: 65%;
 <p>If your domain has remained in this status for several days, you may want to contact your registrar to request information about the delay in processing.</p>
 			<p>If the <abbr title="Top Level Domain">TLD</abbr> requires documentation to be provided for registration, you may need to provide the required documentation.</p>
 </td>
+	</tr>
+
+</tbody>
+</table>
+
+
+<table>
+<tbody>
+<tr class="dark">
+		<th>Status Code</th>
+		<th>What does it mean?</th>
+		<th>Should you do something?</th>
 	</tr>
 	<tr class="light">
 		<td>
@@ -127,6 +148,17 @@ footer{color: green; text-align: center; font-family: verdana; font-size: 65%;
 			<p>If the <abbr title="Top Level Domain">TLD</abbr> is not on a special registration period and you are NOT the listed <abbr title="Registrant">Registrant</abbr>, you should contact your registrar immediately to resolve the issue.</p>
 </td>
 	</tr>
+</tbody>
+</table>
+	
+<table>
+<tbody>
+<tr class="dark">
+		<th>Status Code</th>
+		<th>What does it mean?</th>
+		<th>Should you do something?</th>
+	</tr>
+
 	<tr class="light">
 		<td>
 <a name="pendingdelete" id="pendingdelete"></a><a name="pendingDelete" id="pendingDelete"></a>pendingDelete</td>
@@ -136,43 +168,157 @@ footer{color: green; text-align: center; font-family: verdana; font-size: 65%;
 </td>
 		<td><p>If you want to keep your domain name, you must immediately contact your registrar to discuss what options are available.</p></td>
 	</tr>
-	<tr>
+
+
+</tbody>
+</table>
+
+
+<table>
+<tbody>
+<tr class="dark">
+		<th>Status Code</th>
+		<th>What does it mean?</th>
+		<th>Should you do something?</th>
+	</tr>
+
+
+<tr>
 		<td>
 <a name="pendingrenew" id="pendingrenew"></a><a name="pendingRenew" id="pendingRenew"></a>pendingRenew</td>
 		<td><p>This status code indicates that a request to renew your domain has been received and is being processed.</p></td>
 		<td><p>If you did not request to renew your domain and do not want to keep it (i.e., pay the renewal fee) anymore, you should contact your registrar immediately to discuss what options are available.</p></td>
 	</tr>
-	<tr class="light">
+
+
+</tbody>
+</table>
+
+
+<table>
+<tbody>
+<tr class="dark">
+		<th>Status Code</th>
+		<th>What does it mean?</th>
+		<th>Should you do something?</th>
+	</tr>
+
+
+<tr class="light">
 		<td>
 <a name="pendingrestore" id="pendingrestore"></a><a name="pendingRestore" id="pendingRestore"></a>pendingRestore</td>
 		<td><p>This status code indicates that your registrar has asked the registry to restore your domain that was in redemptionPeriod status. Your registry will hold the domain in this status while waiting for your registrar to provide required restoration documentation. If your registrar fails to provide documentation to the registry operator within a set time period to confirm the restoration request, the domain will revert to redemptionPeriod status.</p></td>
 		<td><p>Watch your domain's status codes within this frequently defined seven day period to ensure that your registrar has submitted the correct restoration documentation within the time window. If this period ended and your domain has reverted back to a redemptionPeriod status, contact your registrar to resolve whatever issues that may have halted the delivery of your domain's required restoration documentation.</p></td>
 	</tr>
-	<tr>
+
+
+
+</tbody>
+</table>
+
+
+<table>
+<tbody>
+<tr class="dark">
+		<th>Status Code</th>
+		<th>What does it mean?</th>
+		<th>Should you do something?</th>
+	</tr>
+
+
+
+<tr>
 		<td>
 <a name="pendingtransfer" id="pendingtransfer"></a><a name="pendingTransfer" id="pendingTransfer"></a>pendingTransfer</td>
 		<td><p>This status code indicates that a request to transfer your domain to a new registrar has been received and is being processed.</p></td>
 		<td><p>If you did not request to transfer your domain, you should contact your registrar immediately to request that they deny the transfer request on your behalf.</p></td>
 	</tr>
-	<tr class="light">
+
+
+
+</tbody>
+</table>
+
+
+<table>
+<tbody>
+<tr class="dark">
+		<th>Status Code</th>
+		<th>What does it mean?</th>
+		<th>Should you do something?</th>
+	</tr>
+
+
+
+<tr class="light">
 		<td>
 <a name="pendingupdate" id="pendingupdate"></a><a name="pendingUpdate" id="pendingUpdate"></a>pendingUpdate</td>
 		<td><p>This status code indicates that a request to update your domain has been received and is being processed.</p></td>
 		<td><p>If you did not request to update your domain, you should contact your registrar immediately to resolve the issue.</p></td>
 	</tr>
-	<tr>
+
+
+
+</tbody>
+</table>
+
+
+<table>
+<tbody>
+<tr class="dark">
+		<th>Status Code</th>
+		<th>What does it mean?</th>
+		<th>Should you do something?</th>
+	</tr>
+
+
+
+<tr>
 		<td>
 <a name="redemptionperiod" id="redemptionperiod"></a><a name="redemptionPeriod" id="redemptionPeriod"></a>redemptionPeriod</td>
 		<td><p>This status code indicates that your registrar has asked the registry to delete your domain. Your domain will be held in this status for 30 days. After five calendar days following the end of the redemptionPeriod, your domain is purged from the registry database and becomes available for registration.</p></td>
 		<td><p>If you want to keep your domain, you must immediately contact your registrar to resolve whatever issues resulted in your registrar requesting that your domain be deleted, which resulted in the redemptionPeriod status for your domain.  Once any outstanding issues are resolved and the appropriate fee has been paid, your registrar should restore the domain on your behalf.</p></td>
 	</tr>
-	<tr class="light">
+
+
+
+
+</tbody>
+</table>
+
+
+<table>
+<tbody>
+<tr class="dark">
+		<th>Status Code</th>
+		<th>What does it mean?</th>
+		<th>Should you do something?</th>
+	</tr>
+
+
+
+<tr class="light">
 		<td>
 <a name="renewperiod" id="renewperiod"></a><a name="renewPeriod" id="renewPeriod"></a>renewPeriod</td>
 		<td><p>This grace period is provided after a domain name registration period is explicitly extended (renewed) by the registrar. If the registrar deletes the domain name during this period, the registry provides a credit to the registrar for the cost of the renewal.</p></td>
 		<td><p>This is an informative status set for a limited period or your domain's renewal by your registrar. If you did not request to renew your domain and do not want to keep it (i.e., pay the renewal fee) anymore, you should contact your registrar immediately to discuss what options are available.</p></td>
 	</tr>
-	<tr>
+
+
+</tbody>
+</table>
+
+
+<table>
+<tbody>
+<tr class="dark">
+		<th>Status Code</th>
+		<th>What does it mean?</th>
+		<th>Should you do something?</th>
+	</tr>
+
+
+<tr>
 		<td>
 <a name="serverdeleteprohibited" id="serverdeleteprohibited"></a><a name="serverDeleteProhibited" id="serverDeleteProhibited"></a>serverDeleteProhibited</td>
 		<td><p>This status code prevents your domain from being deleted. It is an uncommon status that is usually enacted during legal disputes, at your request, or when a redemptionPeriod status is in place.</p></td>
@@ -181,19 +327,74 @@ footer{color: green; text-align: center; font-family: verdana; font-size: 65%;
 			<p>Alternatively, some Registry Operators offer a Registry Lock Service that allows registrants, through their registrars to set this status as an extra protection against unauthorized deletions. Removing this status can take longer than it does for clientDeleteProhibited because your registrar has to forward your request to your domain's registry and wait for them to lift the restriction.</p>
 </td>
 	</tr>
-	<tr class="light">
+
+
+
+
+</tbody>
+</table>
+
+
+<table>
+<tbody>
+<tr class="dark">
+		<th>Status Code</th>
+		<th>What does it mean?</th>
+		<th>Should you do something?</th>
+	</tr>
+
+
+
+
+<tr class="light">
 		<td>
 <a name="serverhold" id="serverhold"></a><a name="serverHold" id="serverHold"></a>serverHold</td>
 		<td><p>This status code is set by your domain's Registry Operator.  Your domain is not activated in the <abbr title="Domain Name System">DNS</abbr>.</p></td>
 		<td><p>If you provided delegation information (name servers), this status may indicate an issue with your domain that needs resolution. If so, you should contact your registrar to request more information. If your domain does not have any issues, but you need it to resolve in the <abbr title="Domain Name System">DNS</abbr>, you must first contact your registrar in order to provide the necessary delegation information.</p></td>
 	</tr>
-	<tr>
+
+
+
+</tbody>
+</table>
+
+
+<table>
+<tbody>
+<tr class="dark">
+		<th>Status Code</th>
+		<th>What does it mean?</th>
+		<th>Should you do something?</th>
+	</tr>
+
+
+
+
+<tr>
 		<td>
 <a name="serverrenewprohibited" id="serverrenewprohibited"></a><a name="serverRenewProhibited" id="serverRenewProhibited"></a>serverRenewProhibited</td>
 		<td><p>This status code indicates your domain's Registry Operator will not allow your registrar to renew your domain. It is an uncommon status that is usually enacted during legal disputes or when your domain is subject to deletion.</p></td>
 		<td><p>Often, this status indicates an issue with your domain that needs to be addressed promptly. You should contact your registrar to request more information and resolve the issue. If your domain does not have any issues, and you simply want to renew it, you must first contact your registrar and request that they work with the Registry Operator to remove this status code. This process can take longer than it does for clientRenewProhibited because your registrar has to forward your request to your domain's registry and wait for them to lift the restriction.</p></td>
 	</tr>
-	<tr class="light">
+
+
+
+</tbody>
+</table>
+
+
+<table>
+<tbody>
+<tr class="dark">
+		<th>Status Code</th>
+		<th>What does it mean?</th>
+		<th>Should you do something?</th>
+	</tr>
+
+
+
+
+<tr class="light">
 		<td>
 <a name="servertransferprohibited" id="servertransferprohibited"></a><a name="serverTransferProhibited" id="serverTransferProhibited"></a>serverTransferProhibited</td>
 		<td><p>This status code prevents your domain from being transferred from your current registrar to another. It is an uncommon status that is usually enacted during legal or other disputes, at your request, or when a redemptionPeriod status is in place.</p></td>
@@ -202,7 +403,26 @@ footer{color: green; text-align: center; font-family: verdana; font-size: 65%;
 			<p>Alternatively, some Registry Operators offer a Registry Lock Service that allows registrants, through their registrars to set this status as an extra protection against unauthorized transfers. Removing this status can take longer than it does for clientTransferProhibited because your registrar has to forward your request to your domain's registry and wait for them to lift the restriction.</p>
 </td>
 	</tr>
-	<tr>
+
+
+
+</tbody>
+</table>
+
+
+<table>
+<tbody>
+<tr class="dark">
+		<th>Status Code</th>
+		<th>What does it mean?</th>
+		<th>Should you do something?</th>
+	</tr>
+
+
+
+
+
+<tr>
 		<td>
 <a name="serverupdateprohibited" id="serverupdateprohibited"></a><a name="serverUpdateProhibited" id="serverUpdateProhibited"></a>serverUpdateProhibited</td>
 		<td><p>This status code locks your domain preventing it from being updated. It is an uncommon status that is usually enacted during legal disputes, at your request, or when a redemptionPeriod status is in place.</p></td>
@@ -211,7 +431,24 @@ footer{color: green; text-align: center; font-family: verdana; font-size: 65%;
 		<p>Alternatively, some Registry Operators offer a Registry Lock Service that allows registrants, through their registrars to set this status as an extra protection against unauthorized updates. Removing this status can take longer than it does for clientUpdateProhibited because your registrar has to forward your request to your domain's registry and wait for them to lift the restriction.</p>
 </td>
 	</tr>
-	<tr class="light">
+
+
+</tbody>
+</table>
+
+
+<table>
+<tbody>
+<tr class="dark">
+		<th>Status Code</th>
+		<th>What does it mean?</th>
+		<th>Should you do something?</th>
+	</tr>
+
+
+
+
+<tr class="light">
 		<td>
 <a name="transferperiod" id="transferperiod"></a><a name="transferPeriod" id="transferPeriod"></a>transferPeriod</td>
 		<td><p>This grace period is provided after the successful transfer of a domain name from one registrar to another.  If the new registrar deletes the domain name during this period, the registry provides a credit to the registrar for the cost of the transfer.</p></td>
