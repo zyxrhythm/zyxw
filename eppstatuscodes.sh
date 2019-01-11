35b1b2d2a2fe6532d9aa425941ef670bbffaa263
Zyx Rhythm <zyxrhythm@gmail.com>
2019-01-12 06:52:46 +0800

    Update eppstatuscodes.sh

--- eppstatuscodes.sh
+++ eppstatuscodes.sh
@@ -268,7 +268,7 @@ footer{color: green; text-align: center; font-family: verdana; font-size: 65%;
 <br>
 <br>
 
-<table </a>id="renewperiod"<a>>
+<table <a>id="renewperiod"</a>>
 <tbody>
 <tr>
 <th>Status Code</th>
