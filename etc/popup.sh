edb42273dd89dee4d3b57b7679231a04bcde5d1c
Zyx Rhythm <zyxrhythm@gmail.com>
2019-01-19 09:52:57 +0800

    Create popup.sh

---
+++ etc/popup.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+#This script was created by Zyx Rhythm (email:zyxrhythm@gmail.com)
+#You can download, copy and use it as you see fit.
+#If ever you need to post or show the code to people
+#Though you are not required to, crediting the the author will be much appreciated.
+################################################
+#https://github.com/zyxrhythm
+#############################################
+
+#start the html header
+echo "Content-type: text/html"
+echo ""
+
+echo '<!DOCTYPE html>'
+
+echo '<html>'
+
+#Tab title
+echo '<title>BBC Special</title>'
+
+#start of head
+echo '<head>'
+echo '<link rel="icon" type="image/png" href="/icon.png" />
+
+
+cat <<EOS
+<script>
+
+$(window).load(function () {
+    $(".trigger_popup_fricc").click(function(){
+       $('.hover_bkgr_fricc').show();
+    });
+    $('.hover_bkgr_fricc').click(function(){
+        $('.hover_bkgr_fricc').hide();
+    });
+    $('.popupCloseButton').click(function(){
+        $('.hover_bkgr_fricc').hide();
+    });
+});
+
+</script>
+
+EOS
+
+cat <<EOC
+
+/* Popup box BEGIN */
+.hover_bkgr_fricc{
+    background:rgba(0,0,0,.4);
+    cursor:pointer;
+    display:none;
+    height:100%;
+    position:fixed;
+    text-align:center;
+    top:0;
+    width:100%;
+    z-index:10000;
+}
+.hover_bkgr_fricc .helper{
+    display:inline-block;
+    height:100%;
+    vertical-align:middle;
+}
+.hover_bkgr_fricc > div {
+    background-color: #fff;
+    box-shadow: 10px 10px 60px #555;
+    display: inline-block;
+    height: auto;
+    max-width: 551px;
+    min-height: 100px;
+    vertical-align: middle;
+    width: 60%;
+    position: relative;
+    border-radius: 8px;
+    padding: 15px 5%;
+}
+.popupCloseButton {
+    background-color: #fff;
+    border: 3px solid #999;
+    border-radius: 50px;
+    cursor: pointer;
+    display: inline-block;
+    font-family: arial;
+    font-weight: bold;
+    position: absolute;
+    top: -20px;
+    right: -20px;
+    font-size: 25px;
+    line-height: 30px;
+    width: 30px;
+    height: 30px;
+    text-align: center;
+}
+.popupCloseButton:hover {
+    background-color: #ccc;
+}
+.trigger_popup_fricc {
+    cursor: pointer;
+    font-size: 20px;
+    margin: 20px;
+    display: inline-block;
+    font-weight: bold;
+}
+/* Popup box BEGIN */
+
+
+EOC
+
+echo '</head>'
+
+cat << EOH
+<body>
+
+<a class="trigger_popup_fricc">Click here to show the popup</a>
+
+<div class="hover_bkgr_fricc">
+    <span class="helper"></span>
+    <div>
+        <div class="popupCloseButton">X</div>
+        <p>Add any HTML content<br />inside the popup box!</p>
+    </div>
+</div>
+
+</body>
+</html>
+
+EOH
+
+exit 0;
+
