034c4f9593c9a89932792357fb8f55d6fc712cad
Zyx Rhythm <zyxrhythm@gmail.com>
2019-01-19 09:59:57 +0800

    Update popup.sh

--- etc/popup.sh
+++ etc/popup.sh
@@ -22,6 +22,10 @@ echo '<title>BBC Special</title>'
 echo '<head>'
 echo '<link rel="icon" type="image/png" href="/icon.png" />
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	
+<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+
 
 cat <<EOS
 <script>
