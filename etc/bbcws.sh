7157b17459a0b5427e918334aeef3b057e4d7af3
Zyx Rhythm <zyxrhythm@gmail.com>
2019-01-22 01:39:49 +0800

    Update bbcws.sh

--- etc/bbcws.sh
+++ etc/bbcws.sh
@@ -45,6 +45,9 @@ EOX
 cat <<EOS2
 
 <script>
+
+//this is a snippet from http://edupala.com/copy-div-content-clipboard/
+
 function copyClipboard() {
   var elm = document.getElementById("divClipboard");
   // for Internet Explorer
@@ -68,12 +71,80 @@ function copyClipboard() {
 
   }
 }
+
+
+
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+} 
+
+// from https://www.w3schools.com/howto/howto_js_tabs.asp
+
 </script>
 EOS2
 
-#CSS
+#CSS 
 cat <<EOS
 <style>
+
+
+/* from: https://www.w3schools.com/howto/howto_js_tabs.asp */
+ /* Style the tab */
+.tab {
+  overflow: hidden;
+  border: 1px solid #ccc;
+  background-color: #f1f1f1;
+}
+
+/* Style the buttons that are used to open the tab content */
+.tab button {
+  background-color: inherit;
+  float: left;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  padding: 14px 16px;
+  transition: 0.3s;
+}
+
+/* Change background color of buttons on hover */
+.tab button:hover {
+  background-color: #ddd;
+}
+
+/* Create an active/current tablink class */
+.tab button.active {
+  background-color: #ccc;
+}
+
+/* Style the tab content */
+.tabcontent {
+  display: none;
+  padding: 6px 12px;
+  border: 1px solid #ccc;
+  border-top: none;
+} 
+
+/* from: https://www.w3schools.com/howto/howto_js_tabs.asp */
+
+
 
 a:link { color: red;  }
 a:active { color: red;  }
@@ -170,7 +241,7 @@ cat <<EODC
 <body>
 <p>
 <br/>
-<div class="code-bg" id="divClipboard">
+<div id="divClipboard">
 <p>
 Not an IP Address / Domain! Sorna.
 </div>
@@ -198,6 +269,7 @@ zyxregistrar=$(whois -H $doi -h $rws);
 
 #<p>Registry Whois Server: ... </p>
 ######################################################
+
 cat <<EOWIR0
 <body id="registryws" >
 
@@ -229,6 +301,7 @@ $zyxregistrar
 </body>
 </html>
 EOWIR1
+
 ######################################################
 
 ;;
