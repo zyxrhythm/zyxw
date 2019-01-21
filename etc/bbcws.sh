7b861265c04675d9f4742088e4bf2b3c9ff5a984
Zyx Rhythm <zyxrhythm@gmail.com>
2019-01-22 03:08:30 +0800

    Update bbcws.sh

--- etc/bbcws.sh
+++ etc/bbcws.sh
@@ -74,7 +74,7 @@ function copyClipboard() {
 
 
 
-function openCity(evt, cityName) {
+function whoisserver(evt, ws) {
   // Declare all variables
   var i, tabcontent, tablinks;
 
@@ -91,7 +91,7 @@ function openCity(evt, cityName) {
   }
 
   // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(cityName).style.display = "block";
+  document.getElementById(ws).style.display = "block";
   evt.currentTarget.className += " active";
 } 
 
@@ -275,6 +275,29 @@ cat <<EOWIR0
 
 <div class="code-bg" id="divClipboard" >
 <p>
+
+<div class="tab">
+  <button class="tablinks" onclick="whoisserver(event, 'Registry')">Registry</button>
+  <button class="tablinks" onclick="whoisserver(event, 'Registrar')">Registrar</button>
+</div>
+
+<!-- Tab content -->
+<div id="Registry" class="tabcontent">
+	<p>
+	<pre>
+	$zyx
+	</pre>
+	</p>
+</div>
+
+<div id="Registrar" class="tabcontent">
+	<p>
+	<pre>
+	$zyxregistrar 
+	</pre>
+	</p>
+</div>
+
 <pre>
 $zyx
 </pre>
