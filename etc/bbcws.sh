d810f8b1d7dbb3a2c5f9b3fc63babe3f94e0b58e
Zyx Rhythm <zyxrhythm@gmail.com>
2019-01-22 05:09:18 +0800

    Update bbcws.sh

--- etc/bbcws.sh
+++ etc/bbcws.sh
@@ -72,6 +72,31 @@ function copyClipboard() {
   }
 }
 
+
+function copyClipboard() {
+  var elm = document.getElementById("divClipboard0");
+  // for Internet Explorer
+
+  if(document.body.createTextRange) {
+    var range = document.body.createTextRange();
+    range.moveToElementText(elm);
+    range.select();
+    document.execCommand("Copy");
+    alert("Copied div content to clipboard");
+  }
+  else if(window.getSelection) {
+    // other browsers
+
+    var selection = window.getSelection();
+    var range = document.createRange();
+    range.selectNodeContents(elm);
+    selection.removeAllRanges();
+    selection.addRange(range);
+    document.execCommand("Copy");
+
+  }
+}
+
 //the snippet above is from http://edupala.com/copy-div-content-clipboard/
 
 
