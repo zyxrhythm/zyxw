5e14d3656ac94f10435af6a7bb7e7ca2c8da9518
Zyx Rhythm <zyxrhythm@gmail.com>
2019-01-24 05:25:52 +0800

    Update bbc.sh

--- etc/bbc.sh
+++ etc/bbc.sh
@@ -112,7 +112,7 @@ function removeSpecialCharacters(string) {
 <input type="submit" formaction="bbcx.sh" style="visibility: hidden; display: none;">
 
 <input id="BBCinput" type="text" onblur="this.value=removeSpaces(this.value); this.value=removeSpecialCharacters(this.value);" onKeyDown="if(event.keyCode==13) this.value=removeSpaces(this.value); if(event.keyCode==13) this.value=removeSpecialCharacters(this.value);" onKeyUp="if(event.keyCode==13) this.value=removeSpecialCharacters(this.value);"name="domain">
-
+<br>
 <button formaction="bbcx.sh">BBC Special</button>
 <button formaction="bbcws.sh">WhoIs</button>
 <button formaction="bbcdc.sh">SSL</button>
@@ -127,8 +127,8 @@ function removeSpecialCharacters(string) {
 <form action="bbcd.sh" method="get">
 <h1>Big Nick Digger</h1>
 <label>Domain:</label><br> 
-
 <input id="BBCinput" type="text" onblur="this.value=removeSpaces(this.value); this.value=removeSpecialCharacters(this.value);" onKeyDown="if(event.keyCode==13) this.value=removeSpaces(this.value); if(event.keyCode==13) this.value=removeSpecialCharacters(this.value);" onKeyUp="if(event.keyCode==13) this.value=removeSpecialCharacters(this.value);" name="domain">
+<br>
 <button type="submit"> D I G </button>
 <select name='record'> 
   <option>MX</option>
