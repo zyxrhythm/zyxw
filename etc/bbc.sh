733e078f464d1a0751ac064520674edfffe1a01f
Zyx Rhythm <zyxrhythm@gmail.com>
2019-01-24 03:56:56 +0800

    Update bbc.sh

--- etc/bbc.sh
+++ etc/bbc.sh
@@ -108,7 +108,7 @@ function removeSpecialCharacters(string) {
 <p>
 
 <form method="get">
-<label>Domain:</label>
+<label>Domain/IP address:</label> <br>
 <input type="submit" formaction="bbcx.sh" style="visibility: hidden; display: none;">
 
 <input id="BBCinput" type="text" onblur="this.value=removeSpaces(this.value); this.value=removeSpecialCharacters(this.value);" onKeyDown="if(event.keyCode==13) this.value=removeSpaces(this.value); if(event.keyCode==13) this.value=removeSpecialCharacters(this.value);" onKeyUp="if(event.keyCode==13) this.value=removeSpecialCharacters(this.value);"name="domain">
