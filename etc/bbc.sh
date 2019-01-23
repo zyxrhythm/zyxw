dbf65c721c67560c9265e77177f5e9f0a639a19e
Zyx Rhythm <zyxrhythm@gmail.com>
2019-01-24 05:44:46 +0800

    Update bbc.sh

--- etc/bbc.sh
+++ etc/bbc.sh
@@ -142,7 +142,7 @@ function removeSpecialCharacters(string) {
 <br> <br>
 <input id="BBCinput" type="text" placeholder="8.8.8.8(default)" onblur="this.value=removeSpaces(this.value); this.value=removeSpecialCharacters(this.value);" onKeyDown="if(event.keyCode==13) this.value=removeSpaces(this.value); if(event.keyCode==13) this.value=removeSpecialCharacters(this.value);" onKeyUp="if(event.keyCode==13) this.value=removeSpecialCharacters(this.value);" name="nameserver">
 <br>
-<code><b>@</b>NS (Optional) : </code>
+<code><strong>@</strong>Name Server (Optional) : </code>
 </form>
 </p>
 
