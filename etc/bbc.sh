1a566722b9eb3c09627d6502368d0ee30632e936
Zyx Rhythm <zyxrhythm@gmail.com>
2019-01-24 04:43:34 +0800

    Update bbc.sh

--- etc/bbc.sh
+++ etc/bbc.sh
@@ -130,7 +130,6 @@ function removeSpecialCharacters(string) {
 
 <input id="BBCinput" type="text" onblur="this.value=removeSpaces(this.value); this.value=removeSpecialCharacters(this.value);" onKeyDown="if(event.keyCode==13) this.value=removeSpaces(this.value); if(event.keyCode==13) this.value=removeSpecialCharacters(this.value);" onKeyUp="if(event.keyCode==13) this.value=removeSpecialCharacters(this.value);" name="domain">
 <button type="submit"> D I G </button>
-<br>
 <select name='record'> 
   <option>MX</option>
   <option>A</option>
@@ -140,10 +139,9 @@ function removeSpecialCharacters(string) {
   <option>SPF</option>
   <option selected>ANY</option>
 </select> <br> <br>
-<code><b>@</b>NS (Optional) : </code>
 
 <input id="BBCinput" type="text" placeholder="8.8.8.8(default)" onblur="this.value=removeSpaces(this.value); this.value=removeSpecialCharacters(this.value);" onKeyDown="if(event.keyCode==13) this.value=removeSpaces(this.value); if(event.keyCode==13) this.value=removeSpecialCharacters(this.value);" onKeyUp="if(event.keyCode==13) this.value=removeSpecialCharacters(this.value);" name="nameserver">
-
+<code><b>@</b>NS (Optional) : </code>
 </form>
 </p>
 
