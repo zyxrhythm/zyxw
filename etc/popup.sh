e03d488b7ce0d45fa174f7567967aa91c2b4b1b5
Zyx Rhythm <zyxrhythm@gmail.com>
2019-01-28 23:01:11 +0800

    Update popup.sh

--- etc/popup.sh
+++ etc/popup.sh
@@ -116,6 +116,17 @@ cat <<EOC
     font-weight: bold;
 }
 /* Popup box BEGIN */
+
+table { font-family: verdana; border: 2px solid green; font-size: 90%;
+}
+
+th { border: 2px solid green;
+}
+
+td { vertical-align: top; text-align: left; border: 1px solid green;
+}
+
+
 </style>
 
 EOC
