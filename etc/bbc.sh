4f54b07e91df89f66c86e057f57ce8c0005b5415
Zyx Rhythm <zyxrhythm@gmail.com>
2019-01-31 02:37:17 +0800

    Update bbc.sh

--- etc/bbc.sh
+++ etc/bbc.sh
@@ -119,7 +119,7 @@ removes nasty white spaces on the text fields that causes alot of issue
 <input type="submit" formaction="bbcx.sh" style="visibility: hidden; display: none; ">
 
 <input placeholder="Domain" id="BBCinput" type="text" onblur="this.value=removeSpaces(this.value); this.value=removeSpecialCharacters(this.value);" onKeyDown="if(event.keyCode==13) this.value=removeSpaces(this.value); if(event.keyCode==13) this.value=removeSpecialCharacters(this.value);" onKeyUp="if(event.keyCode==13) this.value=removeSpecialCharacters(this.value);"name="domain">
-<button formaction="bbcx.sh">Inspect</button>
+<button formaction="bbcx.sh">Analyze</button>
 <br>
 <button formaction="bbcdc.sh">SSL-check</button>
 
