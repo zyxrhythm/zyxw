786f88b2438eeb7c1151136eb08ab525c03297aa
Zyx Rhythm <zyxrhythm@gmail.com>
2019-02-01 04:21:24 +0800

    Update bbcx.sh

--- etc/bbcx.sh
+++ etc/bbcx.sh
@@ -237,10 +237,10 @@ while IFS= read -r line
 do
    eppstat=$( echo ${line#*#} | awk '{print tolower($0)}');
    parsedtable=$(cat ./eppstatuscodes.sh | sed -n '/^<!--tag'"$eppstat"'0-->/,/^<!--tag'"$eppstat"'1-->/p;/^<!--tag'"$eppstat"'1-->/q;');
-   echo  "</br> <a class="trigger_popup_fricc" "color:tomato">[?]</a> ${line#*#}";
-   echo "<div class="hover_bkgr_fricc" style="display:none">
+   echo  "</br> <a class="trigger_popup_fricc" style="color:tomato">[?]</a> ${line#*#}";
+   echo "<div class="hover_bkgr_fricc">
          
-         <div style="display:none">
+         <div>
          <div class="popupCloseButton">X</div>
          <p>$parsedtable</p>
          </div>
