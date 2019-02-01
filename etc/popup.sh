53ce4f66dc5feeb0ec74d0f91cede878ce6cf1d7
Zyx Rhythm <zyxrhythm@gmail.com>
2019-02-01 08:15:56 +0800

    Update popup.sh

--- etc/popup.sh
+++ etc/popup.sh
@@ -162,7 +162,7 @@ cat << JAVSCRIPT1
 <script>
 function myFunction() {
   var x = document.getElementById("$eppstatuscode");
-  if (x.style.display === "none") {
+  if (x.style.display === "block") {
     x.style.display = "none";
   } else {
     x.style.display = "block";
