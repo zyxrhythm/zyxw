8608416438585ffb4a58c839f330db59187ecb6b
Zyx Rhythm <zyxrhythm@gmail.com>
2019-02-05 08:05:37 +0800

    Update bbcx.sh

--- etc/bbcx.sh
+++ etc/bbcx.sh
@@ -159,13 +159,9 @@ dsfunction () {
 while IFS= read -r line
 do
    eppstat=$( echo "${line#*#}" | tr -d '\040\011\012\015' | awk '{print tolower($0)}' );
-   echo "<script>
-          function js$eppstat() { var x = document.getElementById('jsf$eppstat'); 
-          if (x.style.display === 'none') { x.style.display = 'block'; } 
-          else { x.style.display = 'none'; } } 
-          </script>"
-echo  "<br/> <a style='cursor: pointer; color:tomato;' class='button' onclick='js$eppstat()'> [?] </a> ${line#*#}"; 
-  parsedtable="$( cat ./eppstatuscodes.sh | sed -n '/^<!--tag'"$eppstat"'0-->/,/^<!--tag'"$eppstat"'1-->/p;/^<!--tag'"$eppstat"'1-->/q;' )";
+   echo "<script> function js$eppstat() { var x = document.getElementById('jsf$eppstat'); if (x.style.display === 'none') { x.style.display = 'block'; } else { x.style.display = 'none'; } } </script>"
+echo  "<br/> <a style='cursor: pointer; color:tomato;' class='button' onclick='js$eppstat()'> [?] </a> ${line#*#}";
+parsedtable="$( cat ./eppstatuscodes.sh | sed -n '/^<!--tag'"$eppstat"'0-->/,/^<!--tag'"$eppstat"'1-->/p;/^<!--tag'"$eppstat"'1-->/q;' )";
 echo "<div id='jsf$eppstat' style='display:none'> $parsedtable </div>";
 done < <(printf '%s\n' "$1");
 }
