ac5d5e62d5360b1ae7cce5931c30fbe0fa93f8fc
Zyx Rhythm <zyxrhythm@gmail.com>
2019-02-08 03:14:00 +0800

    Update bbcx.sh

--- etc/bbcx.sh
+++ etc/bbcx.sh
@@ -149,7 +149,7 @@ done < <(printf '%s\n' "$1");
 
 #ARRAY
 dsarrayfunc () {
-   parsedtable=$( cat ./eppstatuscodes.sh | awk '/<!--tag'"$1"'0-->/{flag=1;next}/<!--tag'"$1"'1-->/{flag=0}flag');
+   parsedtable=$( cat ./epp.txt | awk '/tag'"$1"'0/{flag=1;next}/tag'"$1"'1/{flag=0}flag');
    
 divisoreya=$(echo "<div id='jsf$1' style='display:none'> $parsedtable </div>" | awk '{gsub("</p>", "");print}' );
 echo "$divisoreya"
