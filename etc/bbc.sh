9cf2caed62257ecf8ecadec172ac7b83ffab23c3
Zyx Rhythm <zyxrhythm@gmail.com>
2019-02-09 07:54:17 +0800

    Update bbc.sh

--- etc/bbc.sh
+++ etc/bbc.sh
@@ -140,7 +140,7 @@ removes nasty white spaces on the text fields that causes alot of issue
 <option value="http://www.cnn.com">CNN</option>
 <option value="http://www.abcnews.com">ABC</option>
 <option value="http://www.cbsnews.com">CBS</option>
-<option value="http://www.foxnews.com">FOX</option>
+<option value="/bbcx.sh">FOX</option>
 </select>
 <script type="text/javascript">
  var urlmenu = document.getElementById( 'menu1' );
