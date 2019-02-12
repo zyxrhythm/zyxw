1037c287dd0f72693eb17b2f1cf5e4ea64e05ee6
Zyx Rhythm <zyxrhythm@gmail.com>
2019-02-12 08:14:44 +0800

    Update bbcx.sh

--- etc/bbcx.sh
+++ etc/bbcx.sh
@@ -125,8 +125,9 @@ gtldlist='+(aarp|abarth|abb|abbott|abbvie|abc|able|abogado|abudhabi|academy|acce
 cctldlist='+(ac|ad|ae|af|ag|ai|al|am|ao|aq|ar|as|at|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bw|by|bz|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cu|cv|cw|cx|cy|cz|de|dj|dk|dm|do|dz|ec|ee|eg|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|om|pa|pe|pf|pg|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sh|si|sk|sl|sm|sn|so|sr|ss|st|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tr|tt|tv|tw|tz|ua|ug|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|za|zm|zw)'
 
 #removes "domain=" from the QUERY_STRING and store it in domain variable
-domain=$(echo "$qs" | cut -f2 -d"=" );
-
+#domain=$(echo "$qs" | cut -f2 -d"=" );
+domain=$(grep -oP '(?<=domain=).*?(?=&)' <<< "$qs");
+sslc=$(grep -oP '(?<=sslc=).*?(?=&)' <<< "$qs");
 #=================
 # FUNCTION HALL
 #=================
@@ -250,8 +251,11 @@ fi
 # END OF FUNCTION HALL
 #=====================
 
+if [[ "$sslc"="1" ]]; then echo "<meta http-equiv="refresh" content="0; URL='https://www.bigblackcactus.com/cgi-bin/bbcd.sh?domain=$domain'" />"; exit 0; 
+else
+
 #checks - if the domain variable  entered is null  / the BBC Special button is clicked without placing anything on the Domain box - then throws a Taylor Swift error
-if [[ -z "$domain" ]]; then
+if [[ -z "$domain" ]] && [[ -z "$sslc" ]]; then
 
 cat <<EOTSE
 <body>
@@ -977,6 +981,8 @@ echo '</div>'
 
 #the end of body
 echo '</body>'
+
+fi
 
 fi
 
