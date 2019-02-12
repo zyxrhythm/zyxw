945d05f8dc2f4dc4cfa7d36326cb48763e2f7587
Zyx Rhythm <zyxrhythm@gmail.com>
2019-02-13 07:24:51 +0800

    Update bbcx.sh

--- etc/bbcx.sh
+++ etc/bbcx.sh
@@ -145,7 +145,7 @@ dsflinkinpark="<br> <a style='color:tomato; cursor: pointer;' class='button' onc
 dsfdiv1="<div id='jsf$eppstat' style='display:none'>"
 dsfdiv2="</div>"
 
-thefuck=" $dsflinkinpark $dsfdiv1 $dsfdiv2"
+thefuck=" $dsflinkinpark "
 
 echo $thefuck
 
