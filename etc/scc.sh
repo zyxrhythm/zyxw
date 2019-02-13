35933f2b4ca5aa53b743a4ab2cf8072d659e6a5a
Zyx Rhythm <zyxrhythm@gmail.com>
2019-02-14 06:11:43 +0800

    Update scc.sh

--- etc/scc.sh
+++ etc/scc.sh
@@ -28,22 +28,7 @@ cat <<EODHEAD0
 <meta name="author" content="Zyx Rhythm">
 <!-- Auto Adjust -->
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-32625644-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'UA-32625644-1');
-</script>
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<script>
-  (adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: "ca-pub-3422469360028179",
-    enable_page_level_ads: true
-  });
-</script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+
 <style>
 a {text-decoration: none; }
 a:link { color: red;  }
