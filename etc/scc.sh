9bc5d595cdd142becb71e47b1bcb7776b29d2a96
Zyx Rhythm <zyxrhythm@gmail.com>
2019-02-14 06:01:02 +0800

    Create scc.sh

---
+++ etc/scc.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+#This script was created by Zyx Rhythm (email:zyxrhythm@gmail.com)
+#You can download, copy and use it as you see fit.
+#If ever you need to post or show the code to people
+#Though you are not required to, crediting the the author will be much appreciated.
+################################################
+#https://github.com/zyxrhythm
+#############################################
+
+#start the html header
+echo "Content-type: text/html"
+echo ""
+echo '<!DOCTYPE html>'
+
+echo '<html>'
+
+#Tab title
+echo '<title>BBC Special</title>'
+
+#start of head
+echo '<head>'
+echo '<link rel="icon" type="image/png" href="/icon.png" />'
+
+cat <<EODHEAD0
+<!-- Site Description -->
+<meta name="description" content="BigBlackCactus.com (BBC) is a website that can look up the whois information of a domain, dig DNS records of domains and sub domains from name servers, etc...">
+<meta name="keywords" content="DIG, DNS, WHOIS, SSL CHECK">
+<meta name="author" content="Zyx Rhythm">
+<!-- Auto Adjust -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-32625644-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-32625644-1');
+</script>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-3422469360028179",
+    enable_page_level_ads: true
+  });
+</script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<style>
+a {text-decoration: none; }
+a:link { color: red;  }
+a:active { color: red;  }
+a:hover { color: red;  }
+a:visited { color: red;  }
+div a:link { color: tomato;  font-size: 90%; }
+div a:active { color: tomato;  font-size: 90%; }
+div a:hover { color: tomato;  font-size: 90%; }
+div a:visited { color: tomato;  font-size: 90%; }
+strong {color: green;
+}
+p  { font-family: verdana; font-size: 85%;
+}
+h1 { font-family: verdana; font-size: 70%;
+}
+body { background-color:black; color:white;
+}
+pre{ white-space: pre-wrap; font-size: 85%; font-family: verdana;
+}
+table { font-family: verdana; border: 2px solid green; font-size: 90%;
+}
+th { border: 2px solid green;
+}
+td { vertical-align: top; text-align: left; border: 1px solid green;
+}
+</style>
+</head>
+
+<body>
+</body>
+<p>
+<br>
+<br>
+<a href="/cgi-bin/bbc.sh">
+<img border="0" alt="Cactus" src="/cactus2.png" width="26%" height="26%">
+</a>
+</p>
+<footer></footer>
+</html>
