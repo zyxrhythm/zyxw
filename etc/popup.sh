f8f9d179b41fe50d77f6ff4f8f4f3f246ef011a2
Zyx Rhythm <zyxrhythm@gmail.com>
2019-02-15 02:23:10 +0800

    Update popup.sh

--- etc/popup.sh
+++ etc/popup.sh
@@ -23,6 +23,37 @@ echo '<head>'
 
 echo '<link rel="icon" type="image/png" href="/icon.png" />'
 
+cat <<EOCSS
+<style>
+a {text-decoration: none; }
+a:link { color: red;  }
+a:active { color: red;  }
+a:hover { color: red;  }
+a:visited { color: red;  }
+div a:link { color: tomato;  font-size: 90%; }
+div a:active { color: tomato;  font-size: 90%; }
+div a:hover { color: tomato;  font-size: 90%; }
+div a:visited { color: tomato;  font-size: 90%; }
+strong {color: green;
+}
+p  { font-family: verdana; font-size: 85%;
+}
+h1 { font-family: verdana; font-size: 70%;
+}
+body { background-color:black; color:white;
+}
+pre{ white-space: pre-wrap; font-size: 85%; font-family: verdana;
+}
+table { font-family: verdana; border: 2px solid green; font-size: 90%;
+}
+th { border: 2px solid green;
+}
+td { vertical-align: top; text-align: left; border: 1px solid green;
+}
+</style>
+
+EOCSS
+
 #<SCRIPT LANGUAGE="JavaScript" SRC="/etc/script.js"></SCRIPT>
 
 dstat="clientDeleteProhibited 
