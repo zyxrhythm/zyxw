86de4612723b5acc931e48e2e47103fa4a8819dc
Zyx Rhythm <zyxrhythm@gmail.com>
2019-02-17 12:43:22 +0800

    Update bbc.sh

--- etc/bbc.sh
+++ etc/bbc.sh
@@ -9,29 +9,24 @@
 echo "Content-type: text/html"
 echo ""
 cat <<EOT
-
 <!DOCTYPE html>
 <html>
 <link rel="icon" type="image/png" href="/icon.png" />
 <head>
 <title>BBC - Big Black Cactus</title>
-
 <meta name="description" content="BigBlackCactus.com (BBC) is a website that can look up the whois information of a domain from whois servers, dig DNS records of domains and sub domains from name servers, etc...">
 <meta name="keywords" content="DIG, DNS, WHOIS, SSL CHECK">
 <meta name="author" content="Zyx Rhythm">
  
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-32625644-1"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-
   gtag('config', 'UA-32625644-1');
 </script>
-
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <script>
   (adsbygoogle = window.adsbygoogle || []).push({
@@ -39,110 +34,80 @@ cat <<EOT
     enable_page_level_ads: true
   });
 </script>
-
 <style>
-
 a {text-decoration: none; }
 a:link { color: green; }
 a:active { color: green; }
 a:hover { color: green; }
 a:visited { color: green; }
-
 h1 {font-family: courier new; font-size: 135%;
 }
-
 h4 { font-family: courier new; font-size: 160%;
 }
-
 body { background-color:black; color:green;
 }
-
 p { font-family: calibri; font-size: 100%;
 }
-
 label { font-family: Trebuchet MS; font-size: 110%;
 }
 	
 button { font-family: Trebuchet MS; font-size: 70%; 
 }
-
 select, option { width: auto; overflow: hidden;
 }
-
 footer { font-family: Georgia; font-size: 100%;
 }
-
 code { font-family: Arial; font-size: 80%;
 }
-
 article	{ font-family: Verdana; font-size: 70%;
 }
-
 </style>
-
 </head>
-
 <body>
-
 <!--
 from https://www.mediacollege.com/internet/javascript/form/remove-spaces.html
 removes nasty white spaces on the text fields that causes alot of issue
 -->
-
 <script>
 function removeSpaces(string) {
  return string.split(' ').join('');
 }
 </script>
-
 <!--
 from https://www.mediacollege.com/internet/javascript/form/remove-spaces.html
 removes nasty white spaces on the text fields that causes alot of issue
 -->
-
 <script>
 function removeSpecialCharacters(string) {
  return string.replace(/[^A-Za-z0-9.-]/g, '');
 }
 </script>
-
 <center>
 <p>
 <h4>
 <a href="/cgi-bin/bbc.sh">Big Black Cactus</a>
 </h4>
 </p>
-
 <!-- BBC Special -->
 <p>
 <form name="BBCsform" method="get" action="bbcx.sh" >
 <label>Specials</label> <br>
 <input type="submit" formaction="bbcx.sh" style="visibility: hidden; display: none; " >
-<input type="submit" type=radio >
-<input placeholder=<input type="submit" formaction="bbcx.sh" style="visibility: hidden; display: none; " >"Domain" id="BBCinput" type="text" onblur="this.value=removeSpaces(this.value); this.value=removeSpecialCharacters(this.value);" onKeyDown="if(event.keyCode==13) this.value=removeSpaces(this.value); if(event.keyCode==13) this.value=removeSpecialCharacters(this.value);" onKeyUp="if(event.keyCode==13 ) this.value=removeSpecialCharacters(this.value);"name="domain">
-
+<input placeholder="Domain" id="BBCinput" type="text" onblur="this.value=removeSpaces(this.value); this.value=removeSpecialCharacters(this.value);" onKeyDown="if(event.keyCode==13) this.value=removeSpaces(this.value); if(event.keyCode==13) this.value=removeSpecialCharacters(this.value);" onKeyUp="if(event.keyCode==13 ) this.value=removeSpecialCharacters(this.value);"name="domain">
 <button formaction="bbcx.sh" >Go</button>
 <br>
 <input type="checkbox" id="sslc" name="sslc"  value="1" /> <label>CS</label>
-
 </form>
 </p>
-
-
 <p>
-
 <!-- WHO YOU -->
 <form action="bbcws.sh" method="get">
 <label>Who You: </label> <br>
-
 <input placeholder="Domain / I.P. address" id="whoyouinput" type="text" onblur="this.value=removeSpaces(this.value); this.value=removeSpecialCharacters(this.value);" onKeyDown="if(event.keyCode==13) this.value=removeSpaces(this.value); if(event.keyCode==13) this.value=removeSpecialCharacters(this.value);" onKeyUp="if(event.keyCode==13) this.value=removeSpecialCharacters(this.value);"name="domain">
-
 <button id="whoyoubtn" type="submit" >Who is</button>
 </form>
-
 </p>
 <br>
-
 <!-- BIG NICK DIGGER -->
 <p>
 <form action="bbcd.sh" method="get">
@@ -169,7 +134,6 @@ function removeSpecialCharacters(string) {
 <code>[<strong> will query @8.8.8.8</strong> (if blank) ]</code>
 </form>
 </p>
-
 <p>
 <br>
 <br>
@@ -177,11 +141,8 @@ function removeSpecialCharacters(string) {
 <img border="0" alt="Cactus" src="/cactus2.png" width="26%" height="26%">
 </a>
 </p>
-
 <p>
-
 </body>
-
 <footer>
 <br>
 [&nbsp;&nbsp;" &nbsp;When in doubt... &nbsp;Logout! &nbsp;"&nbsp;&nbsp;]<br>
@@ -193,7 +154,6 @@ rev.666
 <b>-> <a href=/cgi-bin/directorscut.sh rel="noopener noreferrer" target="_blank">Thanks to</a> <-</b>
 </footer>
 </p>
-
 <p>
 <article>
 The whois tool (WHOYOU) will query both regsitry
@@ -208,17 +168,12 @@ Big Nick Digger will dig from Google's DNS sever (8.8.8.8) by default.
 <a href=/cgi-bin/news.sh rel="noopener noreferrer" target="_blank">[ News ]</a>
 </article>
 </p>
-
 <article>
 <b>Once you GO BLACK, you can never GO BACK!</b>
 <br>
 Copyleft 2018 &copy;
 </article>
-
 </center>
-
 </html>
-
 EOT2
-
 exit 0;
