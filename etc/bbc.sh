444d882bbf6f5827ff21a392d3d84bfcc5029762
Zyx Rhythm <zyxrhythm@gmail.com>
2019-02-20 05:18:41 +0800

    Update bbc.sh

--- etc/bbc.sh
+++ etc/bbc.sh
@@ -121,13 +121,13 @@ function redirect()
 }
 </script>
 
-<label>Specials</label>
+<label>BBC Specials</label>
 <form method="get" name="formation" action="" >
 
 <input type="text" type="submit" name="domain" placeholder="Domain" onblur="this.value=removeSpaces(this.value); this.value=removeSpecialCharacters(this.value);" onKeyDown="if(event.keyCode==13) this.value=removeSpaces(this.value); if(event.keyCode==13) this.value=removeSpecialCharacters(this.value); chgAction( action_name )" onKeyUp="if(event.keyCode==13 ) this.value=removeSpecialCharacters(this.value);">
 
 <button onclick="redirect();" type="submit" >Go</button>
-<br><label>SCC</label>
+<br><label>SSL Check</label><br>(leave blank to analyze domain)
 <input type="checkbox" id="square">
 
 </form>
