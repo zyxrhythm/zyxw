7129472c9c99679e630efb88131dbfbcdf310aa7
Zyx Rhythm <zyxrhythm@gmail.com>
2019-02-22 06:59:07 +0800

    Update bbcx.sh

--- bbcx.sh
+++ bbcx.sh
@@ -483,48 +483,146 @@ echo "<p> <a href='/cgi-bin/bbc.sh' ><<</small> back | track</a> </p>"
 
 ;;
 
-#special result for .ph ccTLD - by providing a link to whois.dot.ph with the domain submitted for query
+#special result for .ph ccTLD 
 ph)
+
+ar=$(dig +short $domain @8.8.8.8);
+mxr=$(dig mx +short $domain @8.8.8.8);
+
+arfrgt=$( arfunction "$ar" );
+
+mrfrgt=$( mrfunction "$mxr");
+
 cat <<EOQPH
 <body>
+<div id="divClipboard">
+
 <p>
-<br>
-<a href='https://whois.dot.ph/?utf8=%E2%9C%93&search=$domain' target='_blank'> Click Here </a>To get the whois info of this .ph domain.
+<br>For the  Whois info of this .ph domain <br>
+Click the link below or copy and paste it on a browser's address bar:  <br> <br>
+<a href='https://whois.dot.ph/?utf8=%E2%9C%93&search=$domain' target='_blank'>https://whois.dot.ph/?utf8=%E2%9C%93&search=$domain</a>
 </p>
+
+<p>
+__________________________ <br> <br>
+<strong>Domain:</strong> $domain <br>
+__________________________ <br>
+<a href='https://securitytrails.com/domain/$domain/history/a' target='_blank' style='font-size: 110%'> &#9960; </a> <strong>A records: </strong>
+<br> <br>
+$arfrgt
+<br>
+__________________________
+<br> <br>
+
+<a href='https://securitytrails.com/domain/$domain/history/mx' target='_blank' style='font-size: 110%'> &#9960; </a> <strong> MX records: </strong>
+<br> <br>
+$mrfrgt
+__________________________
+
+</p>
+</div>
+
 </body>
 </html>
+
 EOQPH
 exit 0;
 
 ;;
 
-#special result for .sg ccTLD - by providing a link to www.sgnic.sg with the domain submitted for query
+
+#special result for .sg ccTLD 
 sg)
+
+ar=$(dig +short $domain @8.8.8.8);
+mxr=$(dig mx +short $domain @8.8.8.8);
+
+arfrgt=$( arfunction "$ar" );
+
+mrfrgt=$( mrfunction "$mxr");
 
 cat <<EOQSG
 <body>
+
+<div id="divClipboard">
+
 <p>
-<br>
-<a href='https://www.sgnic.sg/domain-search.html?SearchKey=$domain' target='_blank'> Click Here </a>To get the whois info of this .sg domain.
+<br>For the  Whois info of this .sg domain <br>
+Click the link below or copy and paste it on a browser's address bar:  <br> <br>
+<a href='https://www.sgnic.sg/domain-search.html?SearchKey=$domain' target='_blank'>https://www.sgnic.sg/domain-search.html?SearchKey=$domain</a>
 </p>
+
+<p>
+__________________________ <br> <br>
+<strong>Domain:</strong> $domain <br>
+__________________________ <br>
+<a href='https://securitytrails.com/domain/$domain/history/a' target='_blank' style='font-size: 110%'> &#9960; </a> <strong>A records: </strong>
+<br> <br>
+$arfrgt
+<br>
+__________________________
+<br> <br>
+
+<a href='https://securitytrails.com/domain/$domain/history/mx' target='_blank' style='font-size: 110%'> &#9960; </a> <strong> MX records: </strong>
+<br> <br>
+$mrfrgt
+__________________________
+
+</p>
+</div>
+
 </body>
 </html>
+
 EOQSG
 exit 0;
 
 ;;
 
-#special result for .vn ccTLD - by providing a link to whois.dot.ph with the domain submitted for query
+#special result for .vn ccTLD 
 vn)
-cat <<EOQPH
+
+ar=$(dig +short $domain @8.8.8.8);
+mxr=$(dig mx +short $domain @8.8.8.8);
+
+arfrgt=$( arfunction "$ar" );
+
+mrfrgt=$( mrfunction "$mxr");
+
+cat <<EOQVN
 <body>
+
+<div id="divClipboard">
 <p>
-<br>
-<a href='http://www.vnnic.vn/en/domain' target='_blank'> Click Here </a>To get the whois info of this .vn domain.
+
+<br>For the  Whois info of this .sg domain <br>
+Click the link below or copy and paste it on a browser's address bar:  <br> <br>
+<a href='http://www.vnnic.vn/en/domain' target='_blank'>http://www.vnnic.vn/en/domain</a>
 </p>
+
+<p>
+__________________________ <br> <br>
+<strong>Domain:</strong> $domain <br>
+__________________________ <br>
+<a href='https://securitytrails.com/domain/$domain/history/a' target='_blank' style='font-size: 110%'> &#9960; </a> <strong>A records: </strong>
+<br> <br>
+$arfrgt
+<br>
+__________________________
+<br> <br>
+
+<a href='https://securitytrails.com/domain/$domain/history/mx' target='_blank' style='font-size: 110%'> &#9960; </a> <strong> MX records: </strong>
+<br> <br>
+$mrfrgt
+__________________________
+
+</p>
+</div>
+
 </body>
 </html>
-EOQPH
+
+EOQVN
 exit 0;
 
 ;;
