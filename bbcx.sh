1a3af1a8c5cd98707b1c233b855b5f13edf51f95
Zyx Rhythm <zyxrhythm@gmail.com>
2019-02-28 23:24:39 +0800

    Update bbcx.sh

--- bbcx.sh
+++ bbcx.sh
@@ -244,7 +244,7 @@ while IFS= read -r line
 do
    mxr1=$(echo  $line | cut -f2 -d" ");
 mxr1a=$(echo $line | cut -f1 -d" ");
-echo "<strong>$mxr1a</strong> &nbsp; $mxr1 <br> ";
+echo "<strong>$mxr1a</strong> &nbsp;$mxr1 <br> ";
    mxr2=$(dig a +short "$mxr1" @8.8.8.8 2>/dev/null);
    if [[ -z "$mxr2" ]]; then echo "<br>Invalid MX record: Does not resolve to an IP address!"; else true; fi;
 if (( $(grep -c . <<<"$mxr2") > 1)); then
