51c5f5f95c40d0726f4b6180449140cf9aa65f0f
Zyx Rhythm <zyxrhythm@gmail.com>
2019-03-04 07:37:50 +0800

    Update bbc.sh

--- etc/bbc.sh
+++ etc/bbc.sh
@@ -111,7 +111,7 @@ function removeSpecialCharacters(string) {
 <center>
 <p>
 
-<a href="/cgi-bin/bbc.sh"><div class="tooltip"><h4>Big Black Cactus</h4><span class="tooltiptext" style="font-size: 70%; font-family: calibri; font: green; ">For comments/suggestions/reactions email: <br> director@bigblackcactus.com</span>
+<a href="/cgi-bin/bbc.sh"><div class="tooltip"><h4>Big Black Cactus</h4><span class="tooltiptext" style="font-size: 70%; font-family: calibri; font: green; "><br>For comments/suggestions/reactions email: <br> director@bigblackcactus.com <br></span>
 </div></a>
 
 
@@ -150,7 +150,7 @@ function redirect()
 <p>
 <!-- WHO YOU -->
 <form action="bbcws.sh" method="get">
-<label><div class="tooltip">[ Who You ]<span class="tooltiptext" style="font-size: 70%; font-family: calibri; font: green; ">For domain names the whois tool (WHOYOU) will query both the registry and the registrar's(if found) whois server/s. And will query ARIN's whois server/s for IP addresses.</span>
+<label><div class="tooltip">[ Who You ]<span class="tooltiptext" style="font-size: 70%; font-family: calibri; font: green; "><br>For domain names the whois tool (WHOYOU) will query both the registry and the registrar's whois server/s (if the registrar whois server is found). <br> <br> And will query ARIN's whois server/s for IP addresses.<br></span>
 </div></label> <br>
 <input placeholder="Domain / I.P. address" id="whoyouinput" type="text" onblur="this.value=removeSpaces(this.value); this.value=removeSpecialCharacters(this.value);" onKeyDown="if(event.keyCode==13) this.value=removeSpaces(this.value); if(event.keyCode==13) this.value=removeSpecialCharacters(this.value);" onKeyUp="if(event.keyCode==13) this.value=removeSpecialCharacters(this.value);"name="domain">
 <button id="whoyoubtn" type="submit" >Who is</button>
@@ -160,7 +160,7 @@ function redirect()
 <!-- BIG NICK DIGGER -->
 <p>
 <form action="bbcd.sh" method="get">
-<label><div class="tooltip">[ Big Nick Digger ]<span class="tooltiptext" style="font-size: 70%; font-family: calibri; font: green; ">To reverse an IP address select PTR from the dropdown. <br> <br> <br> Big Nick Digger will dig from Google's DNS sever (8.8.8.8) by default. </span></div></label>
+<label><div class="tooltip">[ Big Nick Digger ]<span class="tooltiptext" style="font-size: 70%; font-family: calibri; font: green; "><br>To reverse an IP address select PTR from the dropdown. <br> <br>Big Nick Digger will dig from Google's DNS sever (8.8.8.8) by default.<br></span></div></label>
 <br> 
 <input placeholder="Domain / I.P. address" id="BBCinput" type="text" onblur="this.value=removeSpaces(this.value); this.value=removeSpecialCharacters(this.value);" onKeyDown="if(event.keyCode==13) this.value=removeSpaces(this.value); if(event.keyCode==13) this.value=removeSpecialCharacters(this.value);" onKeyUp="if(event.keyCode==13) this.value=removeSpecialCharacters(this.value);" name="domain">
 <br>
