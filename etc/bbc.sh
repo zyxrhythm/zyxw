feefdd9daf2ec74b52aa6ffaadfd2d5a87cb6102
Zyx Rhythm <zyxrhythm@gmail.com>
2019-03-04 09:17:33 +0800

    Update bbc.sh

--- etc/bbc.sh
+++ etc/bbc.sh
@@ -136,7 +136,7 @@ function redirect()
 }
 </script>
 
-<strong><label>[ BBC Specials ]</label></strong>
+<label>[ BBC Specials ]</label>
 <form method="get" name="formation" action="" >
 
 <input type="text" type="submit" name="domain" placeholder="Domain" onblur="this.value=removeSpaces(this.value); this.value=removeSpecialCharacters(this.value);" onKeyDown="if(event.keyCode==13) this.value=removeSpaces(this.value); if(event.keyCode==13) this.value=removeSpecialCharacters(this.value); chgAction( action_name )" onKeyUp="if(event.keyCode==13 ) this.value=removeSpecialCharacters(this.value);">
