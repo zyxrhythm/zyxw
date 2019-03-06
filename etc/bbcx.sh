43850cbdb381b837884f349c4d3812a4693f09a9
Zyx Rhythm <zyxrhythm@gmail.com>
2019-03-07 00:41:11 +0800

    Update bbcx.sh

--- etc/bbcx.sh
+++ etc/bbcx.sh
@@ -648,7 +648,7 @@ then echo "<body>
 <div id='divClipboard'>
 <p>
 <br>
-auDA's whois servers answers solely for whois queries regarding .au domains. And there is a limit on how much whois lookup query an I.P. address can do at a certain period of time, and since this website only have 1 I.P. address, you can either wait for this website's server to be able to query auDA's whois server again and try again after a few minutes, or you can go to  <a href='https://whois.auda.org.au/' target='_blank' >https://whois.auda.org.au/</a> to get the raw whois information.
+auDA's whois servers answers solely for whois queries regarding .au domains. And there is a limit on how much whois lookup query an I.P. address can do at a certain period of time, and since this website only have 1 I.P. address, you can either wait for this website's server to be able to query auDA's whois server again, or you can go to  <a href='https://whois.auda.org.au/' target='_blank' >https://whois.auda.org.au/</a> to get the raw whois information.
 <br> <br>
 __________________________
 <br> <br>";
