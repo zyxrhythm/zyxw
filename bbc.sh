bea34575f7e1126c23767472bc63e22e9c28e25a
Zyx Rhythm <zyxrhythm@gmail.com>
2019-03-08 02:12:25 +0800

    Update bbc.sh

--- bbc.sh
+++ bbc.sh
@@ -143,7 +143,7 @@ function redirect()
 
 <button onclick="redirect();" type="submit" >Go</button>
 <br>
-<code style="font-size:100%" ><div class="tooltip">S. C. C. <span class="tooltiptext"><br>SSL Certificate Checker <br>( tick the checkbox to get the details of the SSL certificate issued for the domain / sub domain)<br><br></span>
+<code style="font-size:100%" ><div class="tooltip">S. C. C. <span class="tooltiptext"><br>SSL Certificate Checker <br><br>( tick the checkbox before clicking go to get the details of the SSL certificate issued for the domain / sub domain)<br><br></span>
 </div> </code>
 <input type="checkbox" id="square">
 </form>
@@ -154,7 +154,7 @@ function redirect()
 <p>
 <!-- WHO YOU -->
 <form action="bbcws.sh" method="get">
-<label><div class="tooltip">[ Who You ]<span class="tooltiptext" style="font-size: 70%; font-family: calibri; font: green; "><br>For domain names the whois tool (WHOYOU) will query both the registry and the registrar's whois server/s <br>(if the registrar whois server is found). <br> <br> And for IP addresses the toll will query ARIN's whois server/s.<br><br></span>
+<label><div class="tooltip">[ Who You ]<span class="tooltiptext" style="font-size: 70%; font-family: calibri; font: green; "><br>For domain names the whois tool (WHOYOU) will query both the registry and the registrar's whois server/s <br>(if the registrar whois server is found). <br> <br> And for IP addresses the tool will query ARIN's whois server/s.<br><br></span>
 </div></label> <br>
 <input placeholder="Domain / I.P. address" id="whoyouinput" type="text" onblur="this.value=removeSpaces(this.value); this.value=removeSpecialCharacters(this.value);" onKeyDown="if(event.keyCode==13) this.value=removeSpaces(this.value); if(event.keyCode==13) this.value=removeSpecialCharacters(this.value);" onKeyUp="if(event.keyCode==13) this.value=removeSpecialCharacters(this.value);"name="domain">
 <button id="whoyoubtn" type="submit" >Who is</button>
