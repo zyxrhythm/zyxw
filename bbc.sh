d3cae094df492a9933892abf61ab441c96b19d79
Zyx Rhythm <zyxrhythm@gmail.com>
2019-03-09 07:07:08 +0800

    Update bbc.sh

--- bbc.sh
+++ bbc.sh
@@ -140,7 +140,7 @@ function redirect()
 
 <button onclick="redirect();" type="submit" >Go</button>
 <br>
-<code style="font-size:100%" ><div class="tooltip">S. C. C. <span class="tooltiptext"><br>SSL Certificate Checker <br><br>( tick the checkbox before you click "Go" to get the details of the SSL certificate issued for the domain / sub domain)<br><br></span>
+<code style="font-size:80%" ><div class="tooltip">S. C. C. <span class="tooltiptext"><br>SSL Certificate Checker <br><br>( tick the checkbox before you click "Go" to get the details of the SSL certificate issued for the domain / sub domain)<br><br></span>
 </div> </code>
 <input type="checkbox" id="square">
 </form>
@@ -152,7 +152,7 @@ function redirect()
 <p>
 <!-- WHO YOU -->
 <form action="bbcws.sh" method="get">
-<label><div class="tooltip">[ Who You ]<span class="tooltiptext" style="font-size: 70%; font-family: calibri; font: green; "><br>Put a domain name / I.P. address on the box below then click the "Who is " Button. <br> <br> For domain names the whois tool "WHOYOU" will query both the registry and the registrar's whois server/s <br>(if the registrar whois server is found). <br> <br> And for IP addresses the tool will query ARIN's whois server/s.<br><br></span>
+<label><div class="tooltip">[ Who You ]<span class="tooltiptext" style="font-size: 70%; font-family: calibri; font: green; "><br>Put a domain name / I.P. address on the box below then click the "Who is" Button. <br> <br> For domain names the whois tool "WHOYOU" will query both the registry and the registrar's whois server/s <br>(if the registrar whois server is found). <br> <br> And for IP addresses the tool will query ARIN's whois server/s.<br><br></span>
 </div> </label> <br>
 <input placeholder="Domain / I.P. address" id="whoyouinput" type="text" onblur="this.value=removeSpaces(this.value); this.value=removeSpecialCharacters(this.value);" onKeyDown="if(event.keyCode==13) this.value=removeSpaces(this.value); if(event.keyCode==13) this.value=removeSpecialCharacters(this.value);" onKeyUp="if(event.keyCode==13) this.value=removeSpecialCharacters(this.value);"name="domain">
 <button id="whoyoubtn" type="submit" >Who is</button>
@@ -162,7 +162,7 @@ function redirect()
 <!-- BIG NICK DIGGER -->
 <p>
 <form action="bbcd.sh" method="get">
-<label><div class="tooltip">[ Big Nick Digger ]<span class="tooltiptext" style="font-size: 70%; font-family: calibri; font: green; "><br> Put a domain name on the box  below then select what type of record would you like to dig from the dropdown then opntionally you can specify from what name server will the tool try to dig from.<br> <br>If the name server field is left blank, by default, Big Nick Digger will dig records from Google's DNS sever (8.8.8.8). <br> <br> To reverse an IP address first select PTR from the dropdown then click the "DIG" button.<br><br></span></div></label>
+<label><div class="tooltip">[ Big Nick Digger ]<span class="tooltiptext" style="font-size: 70%; font-family: calibri; font: green; "><br> Put a domain name on the box below, then select what type of record would you like to dig from the dropdown, opntionally you can specify what name server will the tool dig the records for the doamin from. Then finally click the "DIG" Button. <br> <br>If the name server field is left blank, by default, Big Nick Digger will dig records from Google's DNS sever (8.8.8.8). <br> <br> To reverse an IP address first select PTR from the dropdown then click the "DIG" button.<br><br></span></div></label>
 <br> 
 <input placeholder="Domain / I.P. address" id="BBCinput" type="text" onblur="this.value=removeSpaces(this.value); this.value=removeSpecialCharacters(this.value);" onKeyDown="if(event.keyCode==13) this.value=removeSpaces(this.value); if(event.keyCode==13) this.value=removeSpecialCharacters(this.value);" onKeyUp="if(event.keyCode==13) this.value=removeSpecialCharacters(this.value);" name="domain">
 <br>
