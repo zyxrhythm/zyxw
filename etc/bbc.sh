a4f2436058a8e7b5c330cbb0f8e8e5d4dca1569f
Zyx Rhythm <zyxrhythm@gmail.com>
2019-03-10 04:07:38 +0800

    Update bbc.sh

--- etc/bbc.sh
+++ etc/bbc.sh
@@ -112,7 +112,7 @@ function removeSpecialCharacters(string) {
 <center>
 <p>
 
-<a href="/cgi-bin/bbc.sh"><h4><strong>B</strong>ig <strong>B</strong>lack <strong>C</strong><i class='tooltip'>actus</i><span class="tooltiptext" style="font-size: 70%; font-family: calibri; font: green; ">For comments / suggestions / reactions email  director@bigblackcactus.com</span></h4>
+<a href="/cgi-bin/bbc.sh"><h4><strong>B</strong>ig <strong>B</strong>lack <strong>C</strong><span class='tooltip'>actus</span><span class="tooltiptext" style="font-size: 70%; font-family: calibri; font: green; ">For comments / suggestions / reactions email  director@bigblackcactus.com</span></h4>
 </a>
 
 </p>
