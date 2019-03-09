49d8498e6927af1a8530c3094f1a77fc027707d8
Zyx Rhythm <zyxrhythm@gmail.com>
2019-03-10 02:14:48 +0800

    Update bbcx.sh

--- etc/bbcx.sh
+++ etc/bbcx.sh
@@ -203,7 +203,7 @@ if (x.style.display === 'none') { x.style.display = 'block'; }
 else { x.style.display = 'none'; } } 
 </script>"
 
-echo " <a style='color:tomato; cursor: pointer;' class='button tolltip' onclick='jsnsverbose()'> &#9660; 
+echo " <a style='color:tomato; cursor: pointer;' class='button tooltip' onclick='jsnsverbose()'> &#9660; 
 <span class="tooltiptext" style="font-size: 80%; font-family: calibri; font: green; ">
 <br>Click this to check the IP address/es is associated with the current authoritative name servers and what organization and will also give query ARIN for the possible organization the IP is associated with.<br><br>
 </span></a>"
