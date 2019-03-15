30ec3219ec241e1688108c9fa98e099e914e720f
Zyx Rhythm <zyxrhythm@gmail.com>
2019-03-16 05:38:50 +0800

    Update bbc.sh

--- bbc.sh
+++ bbc.sh
@@ -32,6 +32,17 @@ cat <<EOBBCSH
 </script>
 <!-- Global site tag (gtag.js) - Google Analytics -->
 
+
+<!-- AdSense -->
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-3422469360028179",
+    enable_page_level_ads: true
+  });
+</script>
+<!-- AdSense -->
+
 <style>
 
 a {text-decoration: none; }
