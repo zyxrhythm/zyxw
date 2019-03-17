0462c6ba26b6c3c76bf345e0879fc10ebb343c6a
Zyx Rhythm <zyxrhythm@gmail.com>
2019-03-18 04:01:29 +0800

    Update bbcx.sh

--- etc/bbcx.sh
+++ etc/bbcx.sh
@@ -389,6 +389,7 @@ expd=$(echo "$zyx" | grep -i -e "registry expiry date:");
 #stores the domain's expiration date fromt the registrar
 expd0=$(echo "$zyx2" | grep -i -e "registrar registration expiration date:");
 expd1=${expd0/Registration };
+expd2=( echo "$expd1" | sed 's/T/Time: /g');
 
 #stores the domain's creation date
 creationdate=$(echo "$zyx" | grep -i -e "creation date:");
@@ -436,7 +437,7 @@ cat <<EODEDCDGT
 $creationdate
 <br>
 $expd <br>
-$expd1
+$expd2
 <br>
 __________________________
 <br> 
