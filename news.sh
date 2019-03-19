0012865bd3e9f01c0ef98019399d6ffa1c25bb82
Zyx Rhythm <zyxrhythm@gmail.com>
2019-03-20 03:01:01 +0800

    Update news.sh

--- news.sh
+++ news.sh
@@ -167,7 +167,7 @@ pre{
 ------------------------------------
 The above are just notable updates made on the source codes,
 most of the minor updates are not included,
-you can check out <a href="https://github.com/zyxrhythm/bbclive">github</a> for those.
+you can check <a href="https://github.com/zyxrhythm/bbclive">github</a> for those.
 ------------------------------------
 
 
