03bb655eae64ed17c5b84e03bceb425387c013dd
Zyx Rhythm <zyxrhythm@gmail.com>
2019-03-20 01:57:28 +0800

    Update bbc.sh

--- etc/bbc.sh
+++ etc/bbc.sh
@@ -142,7 +142,7 @@ function removeSpecialCharacters(string) {
 <p><a href="/cgi-bin/bbc.sh" ><h4><strong>B</strong>ig <strong>B</strong>lack <strong>C</strong>ac&#127797;us</h4></a><br><br>
 <span id="navigation">
 &#10092;
-<a href=/cgi-bin/moreinfo.sh rel="noopener noreferrer" target="_blank" id="navig-link">ABOUT</a>
+<a href=/cgi-bin/moreinfo.sh rel="noopener noreferrer" target="_blank" id="navig-link">About</a>
 &#10093;
 &nbsp; &nbsp; &nbsp;
 &#10092;
