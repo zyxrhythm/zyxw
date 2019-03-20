37ec90edd031d293be8b2420a0a5c18be935c170
Zyx Rhythm <zyxrhythm@gmail.com>
2019-03-20 11:01:37 +0800

    Update bbc.sh

--- bbc.sh
+++ bbc.sh
@@ -19,12 +19,15 @@ cat <<EOBBCSH
 <html>
 <link rel="icon" type="image/png" href="/icon.png" />
 <head>
+
 <title>BBC - Big Black Cactus</title>
+
 <meta name="description" content="BigBlackCactus.com (BBC) is a website that can look up the whois information of a domain from whois servers, dig DNS records of domains and sub domains from name servers, etc...">
 <meta name="keywords" content="DIG, DNS, WHOIS, SSL CHECK">
 <meta name="author" content="Zyx Rhythm">
  
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-32625644-1"></script>
 <script>
@@ -34,6 +37,8 @@ cat <<EOBBCSH
   gtag('config', 'UA-32625644-1');
 </script>
 <!-- Global site tag (gtag.js) - Google Analytics -->
+
+
 <!-- AdSense -->
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <script>
@@ -43,7 +48,9 @@ cat <<EOBBCSH
   });
 </script>
 <!-- AdSense -->
+
 <style>
+
 a:link { color: green; }
 a:active { color: green; }
 a:hover { color: green; }
@@ -70,11 +77,13 @@ article	{ font-family: Verdana; font-size: 70%;
 }
 strong {font-size: 143%; 
 }
+
 .tooltip {
   position: relative;
   display: inline-block;
   border-bottom: 1px dotted black;
 }
+
 .tooltip .tooltiptext {
   font-size:85%;
   visibility: hidden;
@@ -89,36 +98,52 @@ strong {font-size: 143%;
   position: absolute;
   z-index: 1;
 }
+
 .tooltip:hover .tooltiptext {
   visibility: visible;
 }
+
 #revisions {color: #186a3b
 }
+
 #goblack {color: #1c2833
 }
+
 #navigation { font-family: Arial; font-size: 85%; font-weight: bold; color: #145a32;
 }
+
 #navig-link { text-decoration: none; color: #145a32;
 }
+
 #wave7a { color: white; font-size: 90%;
 }
+
 </style>
+
 </head>
+
 <body>
+
 <!--from https://www.mediacollege.com/internet/javascript/form/remove-spaces.html - removes nasty white spaces on the text fields that causes alot of issue-->
+
 <script>
 function removeSpaces(string) {
  return string.split(' ').join('');
 }
 </script>
+
 <!-- from https://www.mediacollege.com/internet/javascript/form/remove-spaces.html - removes nasty white spaces on the text fields that causes alot of issue-->
+
 <script>
 function removeSpecialCharacters(string) {
  return string.replace(/[^A-Za-z0-9.-]/g, '');
 }
 </script>
+
 <center>
+
 <p><a href="/cgi-bin/bbc.sh" ><h4><strong>B</strong>ig <strong>B</strong>lack <strong>C</strong>ac&#127797;us</h4></a><br>
+
 <span id="navigation">
 <a href=/cgi-bin/moreinfo.sh rel="noopener noreferrer" target="_blank" id="navig-link">
 &#10084;About
@@ -131,10 +156,12 @@ function removeSpecialCharacters(string) {
 <div class="tooltip">
 <span id="navig-link"><label>&#9993;Contact</label></span>
 <span class="tooltiptext" style="font-size: 80%; font-family: calibri; font: green; "><br>For suggestions / comments / reactions<br>email director@bigblackcactus.com<br><br></span>
-</span>
 </div>
+</span>
 </p> <br>
+
 <!-- ################## BBC Special ################# -->
+
 <p><script>
 function redirect()
 { if(document.getElementById("square").checked == true)
@@ -143,38 +170,57 @@ else if(document.getElementById("square").checked == false)
         document.formation.action = "bbcx.sh";       
 }
 </script>
+
 <div class="tooltip">
 <label> &#127797; BBC Special &#127797; </label>
 <span class="tooltiptext" style="font-size: 70%; font-family: calibri; font: green; "><br>Put a domain name (FQDN) in the box below and click the "Go" button - this will generate a report with vital DNS and whois information about the domain.<br><br></span>
 </div>
+
 <form method="get" name="formation" action="" >
+
 <input type="text" type="submit" name="domain" placeholder="Domain / sub domain" onblur="this.value=removeSpaces(this.value); this.value=removeSpecialCharacters(this.value);" onKeyDown="if(event.keyCode==13) this.value=removeSpaces(this.value); if(event.keyCode==13) this.value=removeSpecialCharacters(this.value); chgAction( action_name )" onKeyUp="if(event.keyCode==13 ) this.value=removeSpecialCharacters(this.value);">
+
 <button onclick="redirect();" type="submit" >Go</button><br>
 <label style="font-size:90%" >
+
 <div class="tooltip">
 S. C. C. 
 <span class="tooltiptext"><br>SSL Certificate Checker <br><br>( tick the checkbox before you click "Go" to get details about the SSL certificate issued for a domain / sub domain)<br><br>
 </span>
+
 </div> 
 </label>
+
 <input type="checkbox" id="square">
+
 </form></p><br><br>
+
 <!-- ################## BBC Special ################# -->
+
 <!-- ################## WHO YOU ################# -->
+
 <p><form action="bbcws.sh" method="get">
+
 <label>
 <div class="tooltip">
 &#8284; Who You &#8284;
 <span class="tooltiptext" style="font-size: 70%; font-family: calibri; font: green; "><br>Put a domain name (FQDN) / I.P. address on the box below then click the "Who is" button. <br> <br> For domain names the whois tool "WHOYOU" will query both the registry and the registrar's whois server/s <br>(if the registrar whois server is found). <br> <br> And for IP addresses the tool will query ARIN's whois server/s.<br><br>
 </span>
+
 </div> 
 </label> <br>
+
 <input placeholder="Domain / I.P. address" id="whoyouinput" type="text" onblur="this.value=removeSpaces(this.value); this.value=removeSpecialCharacters(this.value);" onKeyDown="if(event.keyCode==13) this.value=removeSpaces(this.value); if(event.keyCode==13) this.value=removeSpecialCharacters(this.value);" onKeyUp="if(event.keyCode==13) this.value=removeSpecialCharacters(this.value);"name="domain"><br>
 <button id="whoyoubtn" type="submit" >Who is</button>
+
 </form>
 </p>
+
 <!-- ################## WHO YOU ################# -->
+
+
 <!-- ################## BIG NICK DIGGER ################# -->
+
 <p><form action="bbcd.sh" method="get">
 <label>
 <div class="tooltip">
@@ -183,7 +229,9 @@ S. C. C.
 </span>
 </div>
 </label><br> 
+
 <input placeholder="Domain / I.P. address" id="BBCinput" type="text" onblur="this.value=removeSpaces(this.value); this.value=removeSpecialCharacters(this.value);" onKeyDown="if(event.keyCode==13) this.value=removeSpaces(this.value); if(event.keyCode==13) this.value=removeSpecialCharacters(this.value);" onKeyUp="if(event.keyCode==13) this.value=removeSpecialCharacters(this.value);" name="domain"><br><br>
+
 <select name='record'> 
   <option>A</option>
   <option>MX</option>
@@ -196,15 +244,21 @@ S. C. C.
   <option>PTR</option>
   <option selected>ANY</option>
 </select> 
+
 <button type="submit"> D I G </button><br> <br>
+
 <input id="BBCinput" type="text" placeholder="Name Server (Optional)" onblur="this.value=removeSpaces(this.value); this.value=removeSpecialCharacters(this.value);" onKeyDown="if(event.keyCode==13) this.value=removeSpaces(this.value); if(event.keyCode==13) this.value=removeSpecialCharacters(this.value);" onKeyUp="if(event.keyCode==13) this.value=removeSpecialCharacters(this.value);" name="nameserver"><br>
+
 </form></p>
 <!-- ################## BIG NICK DIGGER ################# -->
+
 <p><br><br>
 <a href="/cgi-bin/bbc.sh">
 <img border="0" alt="Cactus" src="/cactus2.png" width="26%" height="26%">
 </a></p>
+
 </body>
+
 <footer>
 <p><div class="tooltip">
 <span id="wave7a" >&nbsp;&nbsp; &nbsp;<label>" [ v-Cntr Jnir 7n (PAK) ] "</label>&nbsp;&nbsp;&nbsp;</span>
@@ -214,7 +268,9 @@ Encoded in ROT 13.
 </div></p>
 <br><br><br><br>
 </footer>
+
 <p><b>-> <a href=/cgi-bin/directorscut.sh rel="noopener noreferrer" target="_blank" style="color: red;">Thanks to</a> <-</b></p>
+
 <p><article>
 All rights reversed. <br>
 This website or any portion thereof may be <br>
@@ -224,12 +280,16 @@ also for the use of brief quotations in a site review.
 <br><br>For suggestions / comments / reactions<br> email director@bigblackcactus.com<br><br><br>
 </article>
 <span id="revisions">rev. $revcount</span></p>
+
 <article>
 <span id="goblack"><b>Once you GO BLACK, you can never go back!</b><span> <br>
 <span style="color: white">Copyleft 2018 &copy;</span>
 </article>
+
 </center>
+
 </html>
+
 EOBBCSH
 
 exit 0;
