0a11026befd4f4dd8639419415103194867d87ca
Zyx Rhythm <zyxrhythm@gmail.com>
2019-03-23 14:55:02 +0800

    Update bbcx.sh

--- etc/bbcx.sh
+++ etc/bbcx.sh
@@ -165,7 +165,7 @@ cat  << EODHEAD1
 </form>
 </p>
 
-<button onclick="copyClipboard()" class="tooltip">Copy Results</button> <span class='tooltiptext' style='font-size: 95%; font-family: calibri; font: green; '> <br> Click the button to copy the results - then simply do a 'paste' on your text editor or note taking app. (expanded tables will be included on the copied result) <br><br></span>
+<button onclick="copyClipboard()" >Copy Results</button> <span class="tooltip">&#128072; </span><span class='tooltiptext' style='font-size: 95%; font-family: calibri; font: green; '> <br> Click the button to copy the results - then simply do a 'paste' on your text editor or note taking app. (expanded tables will be included on the copied result) <br><br></span>
 <hr>
 </head>
 EODHEAD1
