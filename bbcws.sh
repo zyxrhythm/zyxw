9a3ccb5dc5580b143bf51642c310f70a51ab2777
Zyx Rhythm <zyxrhythm@gmail.com>
2019-03-28 21:31:11 +0800

    Update bbcws.sh

--- bbcws.sh
+++ bbcws.sh
@@ -214,6 +214,13 @@ pre { white-space: pre-wrap;font-family: verdana; font-size: 85%;
 strong { color:green;
 }
 
+table { font-family: verdana; border: 2px solid green; font-size: 90%;
+}
+th { border: 2px solid green;
+}
+td { vertical-align: top; text-align: left; border: 1px solid green;
+}
+
 .tooltip {
   position: relative;
   display: inline-block;
