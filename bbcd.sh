2ff8c0a91a4318a9922f19d89723327def9c3dfc
Zyx Rhythm <zyxrhythm@gmail.com>
2019-03-28 23:17:49 +0800

    Update bbcd.sh

--- bbcd.sh
+++ bbcd.sh
@@ -157,7 +157,7 @@ else { x.style.display = 'none'; } }
 
 </td> </tbody> </table> </div></p>
 
-<button onclick="copyClipboard()">Copy Results</button> <label class="tooltip"> &#128072; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<span class='tooltiptext' style='font-size: 95%; font-family: calibri; font: green; '> <br> Click the button to copy the results - then simply do a 'paste' on your text editor or note taking app. <br><br></span></label>
+<button onclick="copyClipboard()">Copy Result</button> <label class="tooltip"> &#128072; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<span class='tooltiptext' style='font-size: 95%; font-family: calibri; font: green; '> <br> Click the button to copy the results - then simply do a 'paste' on your text editor or note taking app. <br><br></span></label>
 <hr> </head>
 ENDOFHTMLHEAD
 
