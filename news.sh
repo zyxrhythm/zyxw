62d48e8055d6ff56305af6476d5cf21cb7d3666a
Zyx Rhythm <zyxrhythm@gmail.com>
2019-03-28 13:55:15 +0800

    Update news.sh

--- news.sh
+++ news.sh
@@ -72,6 +72,8 @@ pre{
 
 -----------------------
 
+- Some improvements on Who You and Big Nick Digger results.
+
 - Fixed Bug: about registrar whois server not found or invalid yet the footer displays no error.
 
 - Fixed bug: days left counter til the registrar expiration date on BBC special result displays 0, the whois server is not found but the counter script still calculated the days left. So instead of a numeric value, the counter will now display an error message instead. 
