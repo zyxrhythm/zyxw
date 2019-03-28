eddf935f5802e794de5897f3a1a58d3ba86a18d0
Zyx Rhythm <zyxrhythm@gmail.com>
2019-03-28 21:30:47 +0800

    Update bbcd.sh

--- bbcd.sh
+++ bbcd.sh
@@ -49,6 +49,13 @@ body { background-color:black; color:white;
 }
 pre{ white-space: pre-wrap;font-size: 100%;
 }
+table { font-family: verdana; border: 2px solid green; font-size: 90%;
+}
+th { border: 2px solid green;
+}
+td { vertical-align: top; text-align: left; border: 1px solid green;
+}
+
 strong{ color:green;
 }
 .tooltip {
