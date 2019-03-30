fbffced70b10bb16d1a8b9b4f503cea31fc0c1a2
Zyx Rhythm <zyxrhythm@gmail.com>
2019-03-31 01:31:43 +0800

    Update bbcd.sh

--- bbcd.sh
+++ bbcd.sh
@@ -85,13 +85,19 @@ a:link { color: red;  }
 a:active { color: red;  }
 a:hover { color: red;  }
 a:visited { color: red;  }
-p  {font-family: verdana; font-size: 85%;
+div a:link { color: tomato;  font-size: 90%; }
+div a:active { color: tomato;  font-size: 90%; }
+div a:hover { color: tomato;  font-size: 90%; }
+div a:visited { color: tomato;  font-size: 90%; }
+strong {color: green;
 }
-h1 { font-family: verdana; font-size: 80%;
+p  { font-family: verdana; font-size: 85%; word-wrap: break-word;
+}
+h1 { font-family: verdana; font-size: 70%;
 }
 body { background-color:black; color:white;
 }
-pre{ white-space: pre-wrap;font-size: 100%;
+pre{ white-space: pre-wrap; font-size: 85%; font-family: verdana;
 }
 table { font-family: verdana; border: 2px solid green; font-size: 90%;
 }
@@ -100,13 +106,12 @@ th { border: 2px solid green;
 td { vertical-align: top; text-align: left; border: 1px solid green;
 }
 
-strong{ color:green;
-}
 .tooltip {
   position: relative;
   display: inline-block;
   border-bottom: 1px dotted black;
 }
+
 .tooltip .tooltiptext {
   font-size:85%;
   visibility: hidden;
@@ -122,6 +127,7 @@ strong{ color:green;
   position: absolute;
   z-index: 1;
 }
+
 .tooltip:hover .tooltiptext {
   display: inline;
   visibility: visible;
