dfe4cb0251a08a96184c05d1628b8e5144f483e4
Zyx Rhythm <zyxrhythm@gmail.com>
2019-03-30 14:30:55 +0800

    Update bbcx.sh

--- etc/bbcx.sh
+++ etc/bbcx.sh
@@ -169,8 +169,8 @@ else { x.style.display = 'none'; } }
 
 <div id='navtable' style='display:none; float:right;' ><table> <tbody><td>
 <a href='/cgi-bin/bbcws.sh?domain=$domain'>[ &#128269; Who You ]</a> 
-<a href='/cgi-bin/bbcdc.sh?domain=$domain'>[ &#128195; SCC ]</a>
-<a href='/cgi-bin/bbcd.sh?domain=$domain&record=ANY&nameserver='>[ &#9935; BND ]</a>
+<a href='/cgi-bin/bbcdc.sh?domain=$domain'>[ &#128195; S.C.C. ]</a>
+<a href='/cgi-bin/bbcd.sh?domain=$domain&record=ANY&nameserver='>[ &#9935; B.N.D. ]</a>
 </td> </tbody> </table></div>
 
 &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
