b6ac19ea045468dca09dc76c8cc8bda8dcc4c84f
Zyx Rhythm <zyxrhythm@gmail.com>
2019-04-01 01:57:20 +0800

    Update bbcd.sh

--- etc/bbcd.sh
+++ etc/bbcd.sh
@@ -199,15 +199,15 @@ if [[ -z "$qnameserver" ]]; then qns="@8.8.8.8"; else qns="@$qnameserver"; fi;
 
 #checks if the domain enter is null  or they click the BBC button without placing anything
 if [[ -z "$domain" ]]; then
-cat <<EOTSE
+cat <<EOTS
 <body>
 <di id="divClipboard">
 <p><br><strong>Current Input</strong> : none <br> <br>
-Enter a valid domain name <a href='https://en.wikipedia.org/wiki/Fully_qualified_domain_name' target='_blank'>(FQDN)<a/>.<br><br></p>
+Enter a valid domain name <a href='https://en.wikipedia.org/wiki/Fully_qualified_domain_name' target='_blank'>(FQDN)<a/> / <a href="https://en.wikipedia.org/wiki/IPv4" target="_blank">IPv4 Address</a>.<br><br></p>
 </div>
 </body>
 </html>
-EOTSE
+EOTS
 exit 0;
 
 else
