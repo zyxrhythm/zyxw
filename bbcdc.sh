2ba6adb3f8ab9623d82cf9a62085339a9cc3bcb8
Zyx Rhythm <zyxrhythm@gmail.com>
2019-04-07 02:44:21 +0800

    Update bbcdc.sh

--- bbcdc.sh
+++ bbcdc.sh
@@ -181,6 +181,7 @@ else { x.style.display = 'none'; } }
 &nbsp;
 <a style='color:tomato; cursor: pointer; font-size: 116%; font-family:verdana;' value="Refresh Page" onClick="window.location.href=window.location.href">&#8635;<span style="font-size: 77%;">Refresh Results</span></a>
 
+<hr>
 </head>
 EOHTMLHEAD
 
