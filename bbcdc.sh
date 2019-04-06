c1e11a581f3d95d1de613b785377e2a56c617106
Zyx Rhythm <zyxrhythm@gmail.com>
2019-04-07 02:52:24 +0800

    Update bbcdc.sh

--- bbcdc.sh
+++ bbcdc.sh
@@ -258,7 +258,7 @@ if [[ -z "$domain" ]]; then
 cat <<EOTSE
 <body>
 <di id="divClipboard">
-<p><br><strong>Current Input</strong> : none <br> <br>
+<p><strong>Current Input</strong> : none <br> <br>
 Enter a valid domain name <a href='https://en.wikipedia.org/wiki/Fully_qualified_domain_name' target='_blank'>(FQDN)<a/> / sub domain.<br><br></p>
 </div>
 </body>
