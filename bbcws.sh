5ea41a8459844d271dfd0981ae4e8bbe7922b65f
Zyx Rhythm <zyxrhythm@gmail.com>
2019-04-07 02:43:47 +0800

    Update bbcws.sh

--- bbcws.sh
+++ bbcws.sh
@@ -290,6 +290,7 @@ else { x.style.display = 'none'; } }
 
 <a style='color:tomato; cursor: pointer; font-size: 116%; font-family:verdana;' value="Refresh Page" onClick="window.location.href=window.location.href">&#8635;<span style="font-size: 77%;">Refresh Results</span></a></p>
 
+<hr>
 </head>
 ENDOFHEAD
 
