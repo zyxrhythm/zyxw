f51c1644833ad3fbea396842055c1d567687e79e
zyxrhythm <zyxrhythm@gmail.com>
2019-04-09 00:46:27 +0800

    Update bbcd.sh

--- etc/bbcd.sh
+++ etc/bbcd.sh
@@ -94,7 +94,7 @@ td { vertical-align: top; text-align: left; border: 1px solid green;}
 
 body table { font-family: verdana; border: 2px solid green; font-size: 90%; }
 body th { border: 1px solid green; }
-body td { vertical-align: top; text-align: left; border: 1px solid green;}
+body td { vertical-align: top; text-align: left; border: 1px solid green; word-break:break-all;}
 
 strong {color: green;}
 .tooltip {
