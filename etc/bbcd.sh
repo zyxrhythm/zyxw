93236e587b54118edcd78f9e4b8dd600b122277d
zyxrhythm <zyxrhythm@gmail.com>
2019-04-08 12:04:02 +0800

    Update bbcd.sh

--- etc/bbcd.sh
+++ etc/bbcd.sh
@@ -244,12 +244,13 @@ done < <(printf '%s\n' "$1");
 }
 
 zyxgd=$( cutterfunc "$zyxgd0" );
+
+unnamedfunc () {
 echo "<table><tbody>
 <tr><th><strong>TTL</strong></th>
 <th><strong>Record Type</strong></th>
 <th><strong>Record</strong></th></tr>
 "
-unnamedfunc () {
 while IFS= read -r line
 do
 
