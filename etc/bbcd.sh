cbe9b9c76f342f20cfea0b7b78a36e59834c6c1e
zyxrhythm <zyxrhythm@gmail.com>
2019-04-08 12:12:30 +0800

    Update bbcd.sh

--- etc/bbcd.sh
+++ etc/bbcd.sh
@@ -97,10 +97,10 @@ table { font-family: verdana; border: 2px solid green; font-size: 90%;}
 th { border: 2px solid green;}
 td { vertical-align: top; text-align: left; border: 1px solid green;}
 
-body table { font-family: verdana; border: 2px solid green; font-size: 90%; display:inline; }
-body th { border: 2px solid white; display:inline; }
-body td { vertical-align: top; text-align: left; border: 1px solid green; display:inline; }
-
+body table { font-family: verdana; border: 2px solid green; font-size: 90%; }
+body th { border: 2px solid white; }
+body td { vertical-align: top; text-align: left; border: 1px solid green; }
+body tr {display: inline;}
 strong {color: green;}
 .tooltip {
   position: relative;
