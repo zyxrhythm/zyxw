b7b23633b1101600fe0f20fda66a46cd08a37300
zyxrhythm <zyxrhythm@gmail.com>
2019-04-09 02:14:00 +0800

    Update bbcx.sh

--- bbcx.sh
+++ bbcx.sh
@@ -387,11 +387,9 @@ echo "$daysleft";
 if [[ -z "$domain" ]]; then
 
 cat <<EOTSE
-<body>
-<div id="divClipboard"><p><br><strong>Current Input</strong> : none <br> <br>
+<body><div id="divClipboard"><p><br><strong>Current Input</strong> : none <br> <br>
 Enter a valid domain name <a href='https://en.wikipedia.org/wiki/Fully_qualified_domain_name' target='_blank'>(FQDN)<a/>.<br><br></p>
-</div>
-</body>
+</div></body>
 </html>
 EOTSE
 exit 0;
