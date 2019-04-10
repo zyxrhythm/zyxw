b7e5767c659e9ad930cbed943a427784b6385c2c
Zyx Rhythm <zyxrhythm@gmail.com>
2019-04-11 01:35:55 +0800

    Update bbcx.sh

--- bbcx.sh
+++ bbcx.sh
@@ -146,7 +146,7 @@ td { vertical-align: top; text-align: left; border: 1px solid green;
 }
 #domaintimes {color: green;
 }
-#navlinkz { display:inline; white-space: pre-line;
+#navlinkz { color:tomato; float:right;
 }
 #noselect {
     -webkit-touch-callout: none;
@@ -159,7 +159,6 @@ td { vertical-align: top; text-align: left; border: 1px solid green;
 
 </style>
 
-<p> <a href="/cgi-bin/bbc.sh" style='float:left'>[ &#127968;Home ]</a>
 <script> 
 function jsxtable() { var x = document.getElementById('xtable'); 
 if (x.style.display === 'none') 
@@ -174,20 +173,44 @@ if (x.style.display === 'none')
 else { x.style.display = 'none'; } } 
 </script>
 
-<div id='navtable' style='display:none; float:right;' ><table> <tbody><td>
+<script> 
+function jsxmenu() { var x = document.getElementById('navmenu'); 
+if (x.style.display === 'none') 
+{ x.style.display = 'inline'; } 
+else { x.style.display = 'none'; } } 
+</script>
 
-<a href='/cgi-bin/bbcx.sh?domain=' id='navlinkz'>[ &#127797;BBCS ]</a>
-<a href='/cgi-bin/bbcws.sh?domain=' id='navlinkz'>[ &#128269;WY ]</a>
-<a href='/cgi-bin/bbcdc.sh?domain=' id='navlinkz'>[ &#128195;SCC ]</a>
-<a href='/cgi-bin/bbcd.sh?domain=&record=ANY&nameserver=' id='navlinkz'>[ &#9935;BND ]</a>
+<p><a href="/cgi-bin/bbc.sh" style='float:left'>[ &#127968;Home ]</a>
 
-</td> </tbody> </table></div>
+<div id='navtable' style='display:none; float:right;' >
 
-&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+<a style='font-size: 126%; cursor: pointer; float:right; ' onclick='jsxtable(); jsx2table(); jsxmenu()' > &#9776;</a>
+<br><br>
 
-<a  style='color:tomato; cursor: pointer; font-size:126%; float:right;' onclick='jsxtable(); jsx2table()'> &#9776; </a>
-</p>
-<p><div id='xtable' ><table> <tbody> <td>
+<table id='navlinkz'> <tbody>
+<tr><td><a href='/cgi-bin/bbcx.sh?domain=' >&#8625;BBC Special&#127797;&#8626;</a></td></tr>
+</tbody></table><br>
+
+<table id='navlinkz'> <tbody>
+<tr><td><a href='/cgi-bin/bbcws.sh?domain=' >&#8625;Who You&#128269;&#8626;</a></td></tr>
+</tbody></table><br>
+
+<table id='navlinkz'> <tbody>
+<tr><td><a href='/cgi-bin/bbcdc.sh?domain=' >&#8625;SCC&#128195;&#8626;</a></td></tr>
+</tbody></table><br>
+
+<table id='navlinkz'> <tbody>
+<tr><td><a href='/cgi-bin/bbcd.sh?domain=&record=ANY&nameserver=' >&#8625;BND&#9935&#8626;</a></td></tr>
+</tbody></table><br>
+</div>
+
+<div id='navmenu' >
+<a style='color:tomato; cursor: pointer; font-size:126%; float:right;' onclick='jsxtable(); jsx2table(); jsxmenu();'> &#9776; </a>
+</div>
+
+<br><br>
+
+<div id='xtable' ><table> <tbody> <td>
 <form action="bbcx.sh" method="get">
 <input placeholder="Enter a Domain(FQDN)"  type="text" onblur="this.value=removeSpaces(this.value); this.value=removeSpecialCharacters(this.value);" onKeyDown="if(event.keyCode==13) this.value=removeSpaces(this.value); if(event.keyCode==13) this.value=removeSpecialCharacters(this.value);" onKeyUp="if(event.keyCode==13) this.value=removeSpecialCharacters(this.value);"name="domain">
 <button type="submit" >Go 2</button>
@@ -408,7 +431,7 @@ then
 #the error that pops up when a domain is not valid/ does not exist
 cat <<EONVDE
 <body>
-<p><br><div id="divClipboard">
+<div id="divClipboard">
 <p><strong>Input</strong> : $domain <br> <br>
 Not a valid/registered domain name<a href='https://en.wikipedia.org/wiki/Fully_qualified_domain_name' target='_blank'>(FQDN)</a>.<br> <br>
 For additional info from Who You, click <a href="/cgi-bin/bbcws.sh?domain=$domain" target="_blank" >here.</a></p>
