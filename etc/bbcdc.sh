9a6bd21de89eed22b4f45d3fe956e2b3bc31708b
zyxrhythm <zyxrhythm@gmail.com>
2019-04-15 04:01:09 +0800

    Update bbcdc.sh

--- etc/bbcdc.sh
+++ etc/bbcdc.sh
@@ -95,22 +95,14 @@ div a:link { color: tomato;  font-size: 90%; }
 div a:active { color: tomato;  font-size: 90%; }
 div a:hover { color: tomato;  font-size: 90%; }
 div a:visited { color: tomato;  font-size: 90%; }
-strong {color: green;
-}
-p  { font-family: verdana; font-size: 85%; word-wrap: break-word;
-}
-h1 { font-family: verdana; font-size: 70%;
-}
-body { background-color:black; color:white;
-}
-pre{ white-space: pre-wrap; font-size: 85%; font-family: verdana;
-}
-table { font-family: verdana; border: 2px solid green; font-size: 90%;
-}
-th { border: 2px solid green;
-}
-td { vertical-align: top; text-align: left; border: 1px solid green;
-}
+strong {color: green;}
+p  { font-family: verdana; font-size: 85%; word-wrap: break-word;}
+h1 { font-family: verdana; font-size: 70%;}
+body { background-color:black; color:white;}
+pre{ white-space: pre-wrap; font-size: 85%; font-family: verdana;}
+table { font-family: verdana; border: 2px solid green; font-size: 90%;}
+th { border: 2px solid green;}
+td { vertical-align: top; text-align: left; border: 1px solid green;}
 
 .tooltip {
   position: relative;
@@ -139,7 +131,6 @@ td { vertical-align: top; text-align: left; border: 1px solid green;
   visibility: visible;
 }
 #navlinkz { color:tomato; float:right;}
-}
 </style>
 
 <script> 
