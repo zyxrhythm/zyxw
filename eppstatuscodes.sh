38308edd1ee1dbf12c17bb980536b7caca4d4d49
zyxrhythm <zyxrhythm@gmail.com>
2019-04-15 03:25:41 +0800

    Update eppstatuscodes.sh

--- eppstatuscodes.sh
+++ eppstatuscodes.sh
@@ -45,7 +45,7 @@ footer{color: green; text-align: center; font-family: verdana; font-size: 65%;}
 </head>
 
 <body>
-<p style='color: red; text-decoration: none; font-family: calibri'><small><<</small><input type='button' style='background:none; border:none; font-size:95%' value='back | track' onClick='history.go(-1);'></p>
+<p style='color: red; text-decoration: none; font-family: calibri'><small><<</small><input type='button' style='background:none; border:none; font-size:95%; color: red;' value='back | track' onClick='history.go(-1);'></p>
 <hr>
 <br><br><br><br><br><br>
 <!--tagaddperiod0-->
@@ -497,7 +497,7 @@ footer{color: green; text-align: center; font-family: verdana; font-size: 65%;}
 <hr>
 The tables above are from <a href="https://www.icann.org/resources/pages/epp-status-codes-2014-06-16-en">ICANN's Article</a> about EPP Status codes.
 <hr>
-<p style='color: red; text-decoration: none; font-family: calibri'><small><<</small><input type='button' style='background:none; border:none; font-size:95%' value='back | track' onClick='history.go(-1);'></p>
+<p style='color: red; text-decoration: none; font-family: calibri'><small><<</small><input type='button' style='background:none; border:none; font-size:95%; color: red;' value='back | track' onClick='history.go(-1);'></p>
 <br><br><br><br><br><br>
 <center>
 ******
