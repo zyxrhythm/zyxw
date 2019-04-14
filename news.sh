1ea96df9ee60f2ed300bf444ad04068936eba786
zyxrhythm <zyxrhythm@gmail.com>
2019-04-14 12:52:55 +0800

    Update news.sh

--- news.sh
+++ news.sh
@@ -53,6 +53,8 @@ pre{white-space: pre-wrap;font-size: 100%;}
 
 -----------------------
 
+- Remorked all '<a href='' style='text-decoration: none; color: red;' onClick='history.go(-1);'><small><<</small> back | track</a>' links, they are now technically a 'back button', same as the back button that browsers have, upon click it will take you back to the previous page visited on the current tab.
+
 - SCC (SSL Certificate Checker) should now be able to handle domains and subdomains that for some reason are pointed to CNAME records.
 
 - Changed the server time zone from EDT to GMT.
