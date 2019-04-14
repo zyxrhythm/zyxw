1554bf87b642aea721fee82d7711f329b75706b9
zyxrhythm <zyxrhythm@gmail.com>
2019-04-14 14:28:45 +0800

    Update bbcd.sh

--- etc/bbcd.sh
+++ etc/bbcd.sh
@@ -228,7 +228,7 @@ cat << EORIP0
 <div id="divClipboard">
 <p><strong>IP address :</strong> $xip <br>
 <strong>Domain/Hostname :</strong> Null.
-<br>There is no name found for this IP address in the queried zone.<br>Posible causes:<br>-There was never a name assigned for this I.P. v4 address.<br>It might be one of the reserved I.P. v4 addresses, which can have any name <br>depending on where you are and what network you are connected to.<br>
+<br><br>There is no name found for this IP address in the queried zone.<br>Posible causes:<br>-There was never a name assigned for this I.P. v4 address.<br>-It might be one of the reserved I.P. v4 addresses, which can have any name <br>depending on where you are and what network you are connected to.<br>
 </p></div><hr>
 <p style='color: red; text-decoration: none; font-family: calibri'><small><<</small><input type='button' style='background:none; border:none; font-size:95%' value='back | track' onClick='history.go(-1);'></p>
 </body>
