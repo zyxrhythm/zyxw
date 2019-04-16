f67b0b6b79a45474fb8fce800efb4b5f7bca739b
zyxrhythm <zyxrhythm@gmail.com>
2019-04-17 07:19:49 +0800

    Update bbcx.sh

--- etc/bbcx.sh
+++ etc/bbcx.sh
@@ -463,24 +463,15 @@ tld0=$( echo $domain | rev | cut -d "." -f1 | rev );
 
 case $tld0 in
    $tldlist0)
-if [[ $( echo "$domain" | grep -o "\." | wc -l) -gt "1" ]] && [[ -z "$domhv" ]]; 
-then domvarx="(A <a href='https://en.wikipedia.org/wiki/Generic_top-level_domain'  target='_blank' >gTLD</a> sub domain)"; hubad0="y";
-else domvarx=" " ; hubad0="y"; 
-fi;
+if [[ $( echo "$domain" | grep -o "\." | wc -l) -gt "1" ]] && [[ -z "$domhv" ]]; then domvarx="(A <a href='https://en.wikipedia.org/wiki/Generic_top-level_domain'  target='_blank' >gTLD</a> sub domain)"; hubad0="y"; else domvarx=" " ; hubad0="y"; fi;
 ;;
 
 	$tldlist1)
-if [[ $( echo "${domain#*.}" | grep -o "\." | wc -l) -gt "1" ]] && [[ -z "$domhv" ]]; 
-then domvarx="(A <a href='https://en.wikipedia.org/wiki/Country_code_top-level_domain' target='_blank' >ccTLD</a> sub domain)"; hubad0="y";
-else domvarx=" " ; hubad0="y"; 
-fi;
+if [[ $( echo "${domain#*.}" | grep -o "\." | wc -l) -gt "1" ]] && [[ -z "$domhv" ]]; then domvarx="(A <a href='https://en.wikipedia.org/wiki/Country_code_top-level_domain' target='_blank' >ccTLD</a> sub domain)"; hubad0="y"; else domvarx=" " ; hubad0="y"; fi;
 ;;
 
 	*)
-if [[ $( echo "$domain" | grep -o "\." | wc -l) -lt "1" ]] && [[ -z "$domhv" ]]; 
-then domvarx="(Not a domain name <a href='https://en.wikipedia.org/wiki/Fully_qualified_domain_name' target='_blank'>(FQDN)</a>/ sub domain)"; hubad0="x";
-else domvarx=" " ; hubad0="x"; 
-fi;
+if [[ $( echo "$domain" | grep -o "\." | wc -l) -lt "1" ]] && [[ -z "$domhv" ]]; then domvarx="(Not a domain name <a href='https://en.wikipedia.org/wiki/Fully_qualified_domain_name' target='_blank'>(FQDN)</a>/ sub domain)"; hubad0="x"; else domvarx=" " ; hubad0="x"; fi;
 ;;
 
 if [[ "$hubad0" = "y" ]]; then hubad1=" naked"; else hubad1=""; fi;
