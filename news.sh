7ac0c4c36737f6b29b293329b9179d80ed22c8f3
zyxrhythm <zyxrhythm@gmail.com>
2019-04-17 12:34:25 +0800

    Update news.sh

--- news.sh
+++ news.sh
@@ -53,6 +53,8 @@ pre{white-space: pre-wrap;font-size: 100%;}
 
 -----------------------
 
+- BBC special should now be able to handle subdomains and inform users about mistakenly placing a sub domain / something else as input.
+
 - Added more info about what BBC special, Who You, SCC, and BND does on their landing pages.
 
 - Added a link to the (BETA) BBC Android Web View App on the Big Cactus picture of the homepage. 
