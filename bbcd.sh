9dd2e2129bd2100277e8ec2d848f6ba133831bc5
Zyx Rhythm <zyxrhythm@gmail.com>
2019-04-24 13:34:44 +0800

    Update bbcd.sh

--- bbcd.sh
+++ bbcd.sh
@@ -128,6 +128,7 @@ strong {color: green;}
 #noselect {
     -webkit-touch-callout: none;
     -webkit-user-select: none;
+    -webkit-user-select: none;
     -khtml-user-select: none;
     -moz-user-select: none;
     -ms-user-select: none;
