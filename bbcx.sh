52d65593172bc9963e45efaf95f1dbb7d18e92ca
Zyx Rhythm <zyxrhythm@gmail.com>
2019-05-14 02:50:57 +0800

    Update bbcx.sh

--- bbcx.sh
+++ bbcx.sh
@@ -247,14 +247,15 @@ else
 #query whois about the domain and store the raw output to a variable
 zyx0=$(whois --verbose $domain 2>&1);
 
-if [[ $(test -h /usr/bin/jwhois && echo "y" || echo "n" ) = "n" ]] && [[ $(test -h /usr/bin/whois && echo "x" || echo "y" ) = "y" ]];
+if [[ "$(test -h /usr/bin/whois && echo 'y' || echo 'n' )" = "y" ]] && [[ "$(test -h /usr/bin/jwhois && echo 'y' || echo 'n' )" = "n" ]];
 then 
 zyx=$(echo "$zyx0" | sed -e '1,/Query string:/d' | sed -n '1!p' );
+trywis=$(echo "$zyx0" | grep -i -e "Using server" | sort -u );
 else 
 zyx=$(echo "$zyx0" | sed -e '1,/[Querying/d' | sed -n '1!p' );
+trywis=$(echo "$zyx0" | grep -i -e "Using server" | sort -u );
 fi;
 
-trywis=$(echo "$zyx0" | grep -i -e "Using server" | sort -u );
 nsxx=$(echo "$zyx" | grep -i -e 'Name server:' );
 
 #==========================
