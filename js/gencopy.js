81cd56bf74196fea1427567dc6d8c8a1ceb3d0fe
Zyx Rhythm <zyxrhythm@gmail.com>
2019-05-20 14:46:05 +0800

    Create gencopy.js

---
+++ js/gencopy.js
@@ -0,0 +1,23 @@
+//The javascript that copies the contents of div to clipboard.
+//this is a snippet from http://edupala.com/copy-div-content-clipboard/
+
+function copyClipboard() {
+  var elm = document.getElementById("divClipboard");
+  // for Internet Explorer
+  if(document.body.createTextRange) {
+    var range = document.body.createTextRange();
+    range.moveToElementText(elm);
+    range.select();
+    document.execCommand("Copy");
+    alert("Copied div content to clipboard");
+  }
+  else if(window.getSelection) {
+    // other browsers
+    var selection = window.getSelection();
+    var range = document.createRange();
+    range.selectNodeContents(elm);
+    selection.removeAllRanges();
+    selection.addRange(range);
+    document.execCommand("Copy");
+  }
+}
