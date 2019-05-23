d0f27fbec83973c12c9556c2a758d38084b1c4b6
Zyx Rhythm <zyxrhythm@gmail.com>
2019-05-24 04:36:08 +0800

    Update analyticsandads.js

--- js/analyticsandads.js
+++ js/analyticsandads.js
@@ -1,4 +1,3 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-32625644-1"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
@@ -7,7 +6,7 @@
   gtag('config', 'UA-32625644-1');
 </script>
 
-<!-- AdSense -->
+
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <script>
   (adsbygoogle = window.adsbygoogle || []).push({
