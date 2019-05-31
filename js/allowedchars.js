955dfef21415e616be91ca9832f995db8a5d41c9
Zyx Rhythm <zyxrhythm@gmail.com>
2019-05-31 23:12:24 +0800

    Update allowedchars.js

--- js/allowedchars.js
+++ js/allowedchars.js
@@ -1,9 +1,10 @@
-//from https://www.mediacollege.com/internet/javascript/form/remove-spaces.html 
 //removes nasty white spaces on the text fields that causes alot of issue
-
+//from https://www.mediacollege.com/internet/javascript/form/remove-spaces.html 
 function removeSpaces(string) {
  return string.split(' ').join('');
 }
+//from https://www.mediacollege.com/internet/javascript/form/remove-spaces.html 
+
 
 //limit the characters allowed in the input that prevents some errors on the sever side scripts
 function removeSpecialCharacters(string) {
