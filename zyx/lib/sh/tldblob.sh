b4ff922f70fc09ea49df9243e2e3b8c885495af7
zyxrhythm <zyxrhythm@gmail.com>
2019-06-03 10:35:24 +0800

    Update tldblob.sh

--- zyx/lib/sh/tldblob.sh
+++ zyx/lib/sh/tldblob.sh
@@ -5,5 +5,4 @@ TLDLIST0=$(cat ../zyx/lib/blob/tldlist0.blob | tr -d '\040\011\012\015');
 GTLDLIST=$(cat ../zyx/lib/blob/gtldx.blob | tr -d '\040\011\012\015');
 CCTLDLIST$(cat ../zyx/lib/blob/cctldx.blob | tr -d '\040\011\012\015');
 
-
 ALLTLD="$TLDLISTX|$TLDLISTG|$TLDLIST0";
