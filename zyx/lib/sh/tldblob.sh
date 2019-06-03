09f7218619e2c70d6842869084321952bb89b033
zyxrhythm <zyxrhythm@gmail.com>
2019-06-03 08:57:26 +0800

    mu 1
    mu 1

--- zyx/lib/sh/tldblob.sh
+++ zyx/lib/sh/tldblob.sh
@@ -1,5 +1,5 @@
-TLDLISTX=$(cat ../zyx/tldlistx.txt | tr -d '\040\011\012\015');
-TLDLISTG=$(cat ../zyx/tldlistg.txt | tr -d '\040\011\012\015');
-TLDLIST0=$(cat ../zyx/tldlist0.txt | tr -d '\040\011\012\015');
+TLDLISTX=$(cat ../zyx/lib/blob/tldlistx.blob | tr -d '\040\011\012\015');
+TLDLISTG=$(cat ../zyx/lib/blob/tldlistg.blob | tr -d '\040\011\012\015');
+TLDLIST0=$(cat ../zyx/lib/blob/tldlist0.blob | tr -d '\040\011\012\015');
 
 ALLTLD="$TLDLISTX|$TLDLISTG|$TLDLIST0";
