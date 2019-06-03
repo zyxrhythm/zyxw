532e502bc3db17b2fb2acb52e5e91f854bf1052e
zyxrhythm <zyxrhythm@gmail.com>
2019-06-03 09:08:05 +0800

    ssl
    ssl

--- zyx/lib/sh/tldblob.sh
+++ zyx/lib/sh/tldblob.sh
@@ -2,4 +2,8 @@ TLDLISTX=$(cat ../zyx/lib/blob/tldlistx.blob | tr -d '\040\011\012\015');
 TLDLISTG=$(cat ../zyx/lib/blob/tldlistg.blob | tr -d '\040\011\012\015');
 TLDLIST0=$(cat ../zyx/lib/blob/tldlist0.blob | tr -d '\040\011\012\015');
 
+GTLDLIST=$(cat ../zyx/lib/blob/gtldx.blob | tr -d '\040\011\012\015');
+CCTLDLIST$(cat ../zyx/lib/blob/cctldx.blob | tr -d '\040\011\012\015');
+
+
 ALLTLD="$TLDLISTX|$TLDLISTG|$TLDLIST0";
