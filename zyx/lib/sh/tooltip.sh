3713827a2e6bb209f627c34d78c7067aec2dc7ad
Zyx Rhythm <zyxrhythm@gmail.com>
2019-06-20 05:41:17 +0800

    Update tooltip.sh

--- zyx/lib/sh/tooltip.sh
+++ zyx/lib/sh/tooltip.sh
@@ -3,3 +3,4 @@ HOMEDIPT=$(cat ../zyx/vars/tooltip.vars | gawk '/<-HOMEDIPT/{flag=1;next}/HOMEDI
 HOMESSLT=$(cat ../zyx/vars/tooltip.vars | gawk '/<-HOMESSLT/{flag=1;next}/HOMESSLT->/{flag=0}flag');
 HOMEWHOIST=$(cat ../zyx/vars/tooltip.vars | gawk '/<-HOMEWHOIST/{flag=1;next}/HOMEWHOIST->/{flag=0}flag');
 HOMEDIGT=$(cat ../zyx/vars/tooltip.vars | gawk '/<-HOMEDIGT/{flag=1;next}/HOMEDIGT->/{flag=0}flag');
+HOMECONTACTT=$(cat ../zyx/vars/tooltip.vars | gawk '/<-HOMECONTACTT/{flag=1;next}/HOMECONTACTT->/{flag=0}flag');
