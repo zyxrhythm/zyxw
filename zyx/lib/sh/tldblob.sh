2751ec6c216c52eac1dc8ec58e7cd8413481d61c
Zyx Rhythm <zyxrhythm@gmail.com>
2019-07-13 00:15:21 +0800

    Update tldblob.sh

--- zyx/lib/sh/tldblob.sh
+++ zyx/lib/sh/tldblob.sh
@@ -1,8 +1,22 @@
+############################
+#exclulded TLDS for dip.zyx
 TLDLISTX=$(cat ../zyx/lib/blob/tldlistx.blob | tr -d '\040\011\012\015');
+#general list of TLD that dip.zyx can chew on
 TLDLISTG=$(cat ../zyx/lib/blob/tldlistg.blob | tr -d '\040\011\012\015');
+#TLD list that dip.zyx can chew on bu will spit out a raw whois result
 TLDLIST0=$(cat ../zyx/lib/blob/tldlist0.blob | tr -d '\040\011\012\015');
+############################
 
+############################
+#complete list of gTLDs
 GTLDLIST=$(cat ../zyx/lib/blob/gtldx.blob | tr -d '\040\011\012\015');
+#complete list of ccTLDs
 CCTLDLIST=$(cat ../zyx/lib/blob/cctldx.blob | tr -d '\040\011\012\015');
+############################
 
-ALLTLD="$TLDLISTX|$TLDLISTG|$TLDLIST0";
+############################
+#ALL supported TLDs.
+ALLTLD="$TLDLISTG|$TLDLIST0";
+#ALL TLDs.
+ALLTLDX="$GTLDLIST|$CCTLDLIST"
+############################
