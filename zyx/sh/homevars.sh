24e139d8155d64010a0b5044ec0019394bf7921e
Zyx Rhythm <zyxrhythm@gmail.com>
2019-10-30 12:29:29 +0800

    Update homevars.sh

--- zyx/sh/homevars.sh
+++ zyx/sh/homevars.sh
@@ -1,32 +1,41 @@
-#https://github.com/zyxrhythm (email:zyxrhythm@gmail.com)
-#this feeds the home.zyx script its variables
-
-#Version info on the bottom of the home page
-revcount=$(cat ../zyx/vars/site.vars | gawk '/<-VER/{flag=1;next}/VER->/{flag=0}flag');
-#What displays on browsers tabs
-SITETITLE=$(cat ../zyx/vars/site.vars | gawk '/<-SITE_TITLE/{flag=1;next}/SITE_TITLE->/{flag=0}flag');
-#The big text on the front page for meta tags
-SITEBANNER=$(cat ../zyx/vars/site.vars | gawk '/<-SB_META_TAG/{flag=1;next}/SB_META_TAG->/{flag=0}flag');
-#The big text on the front page
-SITEBANNERX=$(cat ../zyx/vars/site.vars | gawk '/<-SITE_BANNER/{flag=1;next}/SITE_BANNER->/{flag=0}flag');
-#contact email for the admin
-CEMAIL=$(cat ../zyx/vars/site.vars | gawk '/<-CEMAIL/{flag=1;next}/CEMAIL->/{flag=0}flag');
-
-#The four tools:
-#dip.zyx
-DIP=$(cat ../zyx/vars/site.vars | gawk '/<-DIP/{flag=1;next}/DIP->/{flag=0}flag');
-#sslinfo.zyx
-SSLC=$(cat ../zyx/vars/site.vars | gawk '/<-SSLCHECK/{flag=1;next}/SSLCHECK->/{flag=0}flag');
-#whois.zyx
-WHOIS=$(cat ../zyx/vars/site.vars | gawk '/<-WHOIS/{flag=1;next}/WHOIS->/{flag=0}flag');
-#digger.zyx
-DIGGER=$(cat ../zyx/vars/site.vars | gawk '/<-DIGGER/{flag=1;next}/DIGGER->/{flag=0}flag');
-
-#the encoded message below the site image on the footer of the front page
-ROT13=$(cat ../zyx/vars/site.vars | gawk '/<-ROT13/{flag=1;next}/ROT13->/{flag=0}flag' | tr '[A-Za-z]' '[N-ZA-Mn-za-m]' );
-#the message below the ecoded message
-FOOTER_MESSAGE1=$(cat ../zyx/vars/site.vars | gawk '/<-FOOTER_MESSAGE1/{flag=1;next}/FOOTER_MESSAGE1->/{flag=0}flag');
-#the message above the copy left notice
-FOOTER_MESSAGE2=$(cat ../zyx/vars/site.vars | gawk '/<-FOOTER_MESSAGE2/{flag=1;next}/FOOTER_MESSAGE2->/{flag=0}flag');
-
-
+#sometimes I hate scrolling down a lot - so this was bit off the scripts - it basiclaly a portion of the nav menu
+#DO NOT DELETE
+echo "<script> 
+function jsxtable() { var x = document.getElementById('xtable'); 
+if (x.style.display === 'none') 
+{ x.style.display = 'inline'; } 
+else { x.style.display = 'none'; } } 
+</script>
+<script> 
+function jsx2table() { var x = document.getElementById('navtable'); 
+if (x.style.display === 'none') 
+{ x.style.display = 'inline'; } 
+else { x.style.display = 'none'; } } 
+</script>
+<script> 
+function jsxmenu() { var x = document.getElementById('navmenu'); 
+if (x.style.display === 'none') 
+{ x.style.display = 'inline'; } 
+else { x.style.display = 'none'; } } 
+</script>
+<p><a href="/cgi-bin/home.zyx" style='text-decoration: none; color: red; font-family: arial black; float:left;'>[ &#128506;Home ]</a>
+<div id='navtable' style='display:none; float:right;' >
+<a style='font-size: 126%; cursor: pointer; float:right; ' onclick='jsxtable(); jsx2table(); jsxmenu()' > &#9776;</a>
+<br><br>
+<table id='navlinkz'> <tbody>
+<tr><td><a href='/cgi-bin/dip.zyx' >&#8625; $DIP &#8626;</a></td></tr>
+</tbody></table><br>
+<table id='navlinkz'> <tbody>
+<tr><td><a href='/cgi-bin/whois.zyx' >&#8625; $WHOIS &#8626;</a></td></tr>
+</tbody></table><br>
+<table id='navlinkz'> <tbody>
+<tr><td><a href='/cgi-bin/sslinfo.zyx' >&#8625; $SSLC &#8626;</a></td></tr>
+</tbody></table><br>
+<table id='navlinkz'> <tbody>
+<tr><td><a href='/cgi-bin/digger.zyx' >&#8625; $DIGGER &#8626;</a></td></tr>
+</tbody></table><br>
+</div>
+<div id='navmenu' >
+<a style='color:tomato; cursor: pointer; font-size:126%; float:right;' onclick='jsxtable(); jsx2table(); jsxmenu();'> &#9776; </a>
+</div>
+<br><br>";
