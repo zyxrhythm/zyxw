ec10d0282c081ec0206d232d0e525dd78ae98f57
Zyx Rhythm <zyxrhythm@gmail.com>
2019-10-30 12:28:56 +0800

    Update genvars.sh

--- zyx/sh/genvars.sh
+++ zyx/sh/genvars.sh
@@ -1,7 +1,7 @@
 #This feeds the common vars on the /zyx/vars/site.vars
 
 # SITE BANNER - th eBIG TEXT on the very top of the home page
-SITEBANNER=$(cat ../zyx/vars/site.vars | gawk '/<-SITE_BANNER/{flag=1;next}/SITE_BANNER->/{flag=0}flag');
+SITEBANNER=$(cat ../zyx/vars/site.vars | gawk '/<-SB_META_TAG/{flag=1;next}/SB_META_TAG->/{flag=0}flag');
 
 #Name of Tool 1 - special tool - dip.zyx
 DIP=$(cat ../zyx/vars/site.vars | gawk '/<-DIP/{flag=1;next}/DIP->/{flag=0}flag');
